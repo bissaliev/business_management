5e25e4d25d0fd66ceabb73e97e0abff6fe1f18d1
bissaliev <bissaliev21@gmail.com>
2025-04-05 09:10:53 +0400

    auth_service: Добавил модель User

---
+++ auth_service/app/models/users.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from enum import StrEnum
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Status(StrEnum):
+    EMPLOYEE = "сотрудник"
+    MANAGER = "менеджер"
+    ADMIN = "администратор"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(250), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), unique=True, index=True, nullable=False)
+    status: Mapped[Status] = mapped_column(default=Status.EMPLOYEE)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    deleted_at: Mapped[datetime | None] = None
+    team_id: Mapped[int]
