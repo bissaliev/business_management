53c5d2b36b1a1b149ccad2770bf9075b2d875ba2
bissaliev <bissaliev21@gmail.com>
2025-04-05 09:13:39 +0400

    auth_service: Добавил зависимость UserService

---
+++ auth_service/app/routers/dependencies.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import get_session
+from app.services import UserService
+
+
+async def user_service(session: Annotated[AsyncSession, Depends(get_session)]):
+    return UserService(session)
+
+
+UserServiceDeps = Annotated[UserService, Depends(user_service)]
