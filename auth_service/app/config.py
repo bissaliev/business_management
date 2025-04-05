edbd71be1efa2d216f0c02003dd8c5582f3c0a69
bissaliev <bissaliev21@gmail.com>
2025-04-05 09:09:45 +0400

    Создал файл конфигураций проекта

---
+++ auth_service/app/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Setting(BaseSettings):
+    TEAM_HOST: str
+    TEAM_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DB_HOST: str
+    DB_PORT: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    def get_db_postgres_url(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+
+    def get_team_by_code_url(self):
+        return f"http://{self.TEAM_HOST}:{self.TEAM_PORT}/teams/by-code"
+
+
+settings = Setting()
