3ce076971634942c2cafb3911010b73e0682384a
bissaliev <bissaliev21@gmail.com>
2025-04-06 07:21:32 +0400

    auth_service: Изменил наследование Status от (str, Enum) в models.users

--- auth_service/app/models/users.py
+++ auth_service/app/models/users.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from enum import StrEnum
+from enum import Enum
 
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
@@ -7,9 +7,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 
-class Status(StrEnum):
-    EMPLOYEE = "сотрудник"
-    MANAGER = "менеджер"
+class Status(str, Enum):
+    USER = "пользователь"
     ADMIN = "администратор"
 
 
@@ -19,7 +18,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(250), nullable=False)
     email: Mapped[str] = mapped_column(String(150), unique=True, index=True, nullable=False)
-    status: Mapped[Status] = mapped_column(default=Status.EMPLOYEE)
+    status: Mapped[Status] = mapped_column(default=Status.USER)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
     is_active: Mapped[bool] = mapped_column(default=True)
     deleted_at: Mapped[datetime | None] = None
