296c73cb59ce37e9224a7aae7e611ff9d7a1a0c6
bissaliev <bissaliev21@gmail.com>
2025-04-07 08:44:54 +0400

    team_service: Создал файл конфигураций для сервиса

---
+++ teams_service/app/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Setting(BaseSettings):
+    USER_HOST: str
+    USER_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    DB_HOST: str
+    DB_PORT: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    def get_db_postgres_url(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+
+    def get_user_url(self):
+        return f"http://{self.USER_HOST}:{self.USER_PORT}/users"
+
+
+settings = Setting()
