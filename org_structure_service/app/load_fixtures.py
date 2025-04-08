bc66764327d41711879bcdd61a7d02fe8f03c2b1
bissaliev <bissaliev21@gmail.com>
2025-04-08 16:22:24 +0400

    org_structure_service: Изменил скрипт загрузки тестовых данных (Довил два вида ОСП)

--- org_structure_service/app/load_fixtures.py
+++ org_structure_service/app/load_fixtures.py
@@ -1,36 +1,17 @@
 import asyncio
+import json
 
 from app.database import SessionLocal, async_engine
-from app.models import Base, Department, EmployeeManagers, EmployeeStructure, TeamStructure
-
-fixtures = {
-    "team_structure": [
-        {"team_id": 100, "structure_type": "linear"},
-        {"team_id": 200, "structure_type": "functional"},
-        {"team_id": 300, "structure_type": "matrix"},
-    ],
-    "departments": [
-        {"team_id": 100, "name": "Разработка", "parent_department_id": None},
-        {"team_id": 200, "name": "Разработка", "parent_department_id": None},
-        {"team_id": 200, "name": "Тестирование", "parent_department_id": None},
-        {"team_id": 300, "name": "Разработка", "parent_department_id": None},
-    ],
-    "employee_structure": [
-        {"user_id": 2, "department_id": 1, "role": "manager", "manager_id": None},
-        {"user_id": 1, "department_id": 1, "role": "developer", "manager_id": 2},
-        {"user_id": 2, "department_id": 2, "role": "manager", "manager_id": None},
-        {"user_id": 1, "department_id": 2, "role": "developer", "manager_id": 2},
-        {"user_id": 3, "department_id": 3, "role": "tester", "manager_id": None},
-        {"user_id": 2, "department_id": 4, "role": "manager", "manager_id": None},
-        {"user_id": 1, "department_id": 4, "role": "developer", "manager_id": 2},
-    ],
-    "employee_managers": [
-        {"employee_structure_id": 7, "manager_id": 3, "context": "project"},
-    ],
-}
+from app.models import Base, Department, Division, EmployeeManagers, EmployeeStructure, TeamStructure
 
 
-async def load_fixtures():
+def json_to_dict(filename):
+    with open(filename, encoding="utf-8") as file:
+        content = json.load(file)
+        return content
+
+
+async def load_fixtures(data: dict):
     """Асинхронная функция для загрузки данных"""
     async with async_engine.begin() as conn:
         # Создаём таблицы
@@ -39,25 +20,30 @@ async def load_fixtures():
 
     async with SessionLocal() as session:
         # Загружаем TeamStructure
-        for ts_data in fixtures["team_structure"]:
+        for ts_data in data["team_structure"]:
             ts = TeamStructure(**ts_data)
             session.add(ts)
         await session.commit()
 
+        # Divisions
+        for div_data in data["divisions"]:
+            session.add(Division(**div_data))
+        await session.commit()
+
         # Загружаем Departments
-        for dept_data in fixtures["departments"]:
+        for dept_data in data["departments"]:
             dept = Department(**dept_data)
             session.add(dept)
         await session.commit()
 
         # Загружаем EmployeeStructure
-        for emp_data in fixtures["employee_structure"]:
+        for emp_data in data["employee_structure"]:
             emp = EmployeeStructure(**emp_data)
             session.add(emp)
         await session.commit()
 
         # Загружаем EmployeeManagers
-        for mgr_data in fixtures["employee_managers"]:
+        for mgr_data in data["employee_managers"]:
             mgr = EmployeeManagers(**mgr_data)
             session.add(mgr)
         await session.commit()
@@ -65,6 +51,11 @@ async def load_fixtures():
         print("Fixtures loaded successfully!")
 
 
-# Запуск скрипта
 if __name__ == "__main__":
-    asyncio.run(load_fixtures())
+    from pathlib import Path
+
+    current_dir = Path(__file__).parent
+    filepath = current_dir / "fixtures.json"
+    print(filepath)
+    data = json_to_dict(filepath)
+    asyncio.run(load_fixtures(data))
