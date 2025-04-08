021bc17ea575cb2147d6d9bf1d94947253c4d290
bissaliev <bissaliev21@gmail.com>
2025-04-08 06:58:40 +0400

    auth_service: Добавил автоматическое хеширование пароля при создании пользователя в админке

--- auth_service/app/admin/views.py
+++ auth_service/app/admin/views.py
@@ -1,7 +1,14 @@
 from sqladmin import ModelView
 
+from app.auth import get_password_hash
 from app.models.users import User
 
 
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.name, User.email, User.status, User.is_active]
+    column_labels = {"hashed_password": "Password"}
+
+    async def on_model_change(self, data, model, is_created, request) -> None:
+        if is_created:
+            # Hash the password before saving into DB !
+            data["hashed_password"] = get_password_hash(data["hashed_password"])
