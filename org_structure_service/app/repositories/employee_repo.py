bc42ef732453a28b2ebc62bedd9c1346aeda86f6
bissaliev <bissaliev21@gmail.com>
2025-04-10 09:45:38 +0400

    org_structure_service: Добавил метод получения работника по department_id

--- org_structure_service/app/repositories/employee_repo.py
+++ org_structure_service/app/repositories/employee_repo.py
@@ -8,8 +8,15 @@ class EmployeeRepository(BaseRepository):
     model = EmployeeStructure
 
     async def exists_in_department(self, employee_id: int, department_id: int):
+        """Проверка на существование связи работник-департамент"""
         stmt = select(
             exists(self.model).where(self.model.employee_id == employee_id, self.model.department_id == department_id)
         )
         result = await self.session.execute(stmt)
         return result.scalar()
+
+    async def get_department_employees(self, department_id: int) -> list[EmployeeStructure]:
+        """Получить список сотрудников по идентификатору департамента"""
+        stmt = select(self.model).where(self.model.department_id == department_id)
+        result = await self.session.scalars(stmt)
+        return result.all()
