3e88d15c742a1e6cc7731ee31549e28fbdb2bfda
bissaliev <bissaliev21@gmail.com>
2025-04-10 09:28:35 +0400

    org_structure_service: Добавил эндпоинты для работников получить список и создать работника

---
+++ org_structure_service/app/routers/employees.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database import get_session
+from app.models import EmployeeManagers
+from app.routers.dependencies import EmployeeServiceDeps
+from app.schemas.employees import EmployeeManagerCreate, EmployeeResponse, EmployeeStructureCreate
+
+router = APIRouter()
+
+
+@router.get("/", summary="Список всех сотрудников")
+async def get_employees(employee_service: EmployeeServiceDeps) -> list[EmployeeResponse]:
+    employees = await employee_service.get_employees()
+    return employees
+
+
+@router.post("/", summary="Добавление сотрудника")
+async def add_employee_to_structure(
+    employee: EmployeeStructureCreate, employee_service: EmployeeServiceDeps
+) -> EmployeeResponse:
+    new_employee = await employee_service.add_employee(employee.model_dump(exclude_unset=True))
+    return new_employee
+
+
+@router.post("/managers/", summary="Добавление дополнительного руководителя")
+async def add_employee_manager(
+    manager: EmployeeManagerCreate, session: Annotated[AsyncSession, Depends(get_session)]
+) -> dict:
+    db_mgr = EmployeeManagers(
+        employee_structure_id=manager.employee_structure_id, manager_id=manager.manager_id, context=manager.context
+    )
+    session.add(db_mgr)
+    await session.commit()
+    return {"message": "Manager added"}
