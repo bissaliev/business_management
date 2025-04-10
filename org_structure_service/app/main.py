17edf7733be3c8014fca9a1c892c2849f5278fd8
bissaliev <bissaliev21@gmail.com>
2025-04-10 09:30:10 +0400

    org_structure_service: Добавил маршруты работников в главный модуль

--- org_structure_service/app/main.py
+++ org_structure_service/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from app.routers.departments import router as department_router
+from app.routers.employees import router as employee_router
 from app.routers.structures import router as structure_router
 
 app = FastAPI()
 
 app.include_router(structure_router, prefix="/structures", tags=["structures"])
 app.include_router(department_router, prefix="/departments", tags=["departments"])
+app.include_router(employee_router, prefix="/employees", tags=["employees"])
 
 
 if __name__ == "__main__":
