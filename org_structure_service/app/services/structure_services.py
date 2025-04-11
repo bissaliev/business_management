354e7e6a1149d355b3ee859a827e2c6d6863a327
bissaliev <bissaliev21@gmail.com>
2025-04-11 11:41:53 +0400

    org_structure_service: Добавил TeamServiceClient в логику сервиса OrgStructureService

--- org_structure_service/app/services/structure_services.py
+++ org_structure_service/app/services/structure_services.py
@@ -1,8 +1,7 @@
-import httpx
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.config import settings
+from app.clients.team_client import TeamServiceClient
 from app.repositories.team_structure_repo import TeamStructureRepository
 from app.schemas.team_structures import TeamStructureResponse
 
@@ -13,31 +12,24 @@ class OrgStructureService:
     def __init__(self, session: AsyncSession):
         self.session = session
         self.repo = TeamStructureRepository(session)
+        self.team_client = TeamServiceClient()
 
     async def get_team_structure_all(self):
         team_structures = await self.repo.get_all()
         return team_structures
 
     # TODO: Поправить правильную обработку запросов httpx.AsyncClient
-    async def exists_team_on_team_service(self, team_id: int):
+    async def get_team_on_team_service(self, team_id: int):
         """Проверка на существование команды в team_service"""
-        base_team_url = settings.get_team_service__url()
-        url = f"{base_team_url}/{team_id}/"
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(url)
-                if response.status_code != 200:
-                    return None
-                return response.json()
-        except httpx.ConnectError:
-            return True
+        team = await self.team_client.get_team(team_id)
+        return team
 
     # TODO: Подумать о том чтобы создавать команду на этом сервисе в момент создания в team service
     async def create_team_structure(self, structure_data: dict):
         """Создание организационной структуры команды"""
         team_id = structure_data["team_id"]
-        is_exists_team = await self.exists_team_on_team_service(team_id)
-        if is_exists_team is None:
+        team = await self.get_team_on_team_service(team_id)
+        if team is None:
             raise HTTPException(status_code=404, detail=f"Команда с id={team_id} еще не зарегистрирована")
         if await self.repo.exists(team_id):
             raise HTTPException(status_code=400, detail=f"Структура команды с team_id={team_id} уже существует")
