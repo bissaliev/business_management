47681c20c7b8fd52c22ee6c382b23992c0146bca
bissaliev <bissaliev21@gmail.com>
2025-04-11 16:13:10 +0400

    org_structure_service: Добавил маршруты для получения списка и одного отдела

--- org_structure_service/app/routers/departments.py
+++ org_structure_service/app/routers/departments.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
 from app.routers.dependencies import DepartmentServiceDeps
-from app.schemas.departments import (
-    DepartmentCreate,
-    DepartmentResponse,
-)
+from app.schemas.departments import DepartmentCreate, DepartmentResponse, DepartmentUpdate
 
 router = APIRouter()
+
+
+@router.get("/", summary="Получение списка всех отделов")
+async def get_departments(department_service: DepartmentServiceDeps) -> list[DepartmentResponse]:
+    return await department_service.get_departments()
 
 
 @router.post("/", summary="Создание отдела")
@@ -15,3 +17,15 @@ async def create_department(
 ) -> DepartmentResponse:
     new_department = await department_service.create_department(department.model_dump())
     return new_department
+
+
+@router.get("/{id}", summary="Получение отдела")
+async def get_department(id: int, department_service: DepartmentServiceDeps) -> DepartmentResponse:
+    return await department_service.get_department(id)
+
+
+@router.put("/{id}", summary="Обновление отдела")
+async def update_department(
+    id: int, update_data: DepartmentUpdate, department_service: DepartmentServiceDeps
+) -> DepartmentResponse:
+    return await department_service.update_department(id, update_data.model_dump(exclude_unset=True))
