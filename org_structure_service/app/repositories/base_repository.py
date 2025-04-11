92bd938f550b1a30a6e9a746109a7ad1bd73cb7a
bissaliev <bissaliev21@gmail.com>
2025-04-11 07:01:26 +0400

    org_structure_service: Добавил в BaseRepository аннотации

--- org_structure_service/app/repositories/base_repository.py
+++ org_structure_service/app/repositories/base_repository.py
@@ -1,4 +1,3 @@
-import abc
 from typing import TypeVar
 
 from sqlalchemy import exists, insert, select, update
@@ -9,32 +8,32 @@ from app.database import Base
 ModelType = TypeVar("ModelType", bound=Base)
 
 
-class BaseRepository(abc.ABC):
+class BaseRepository:
     model: type[ModelType]
 
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def add(self, data: dict):
+    async def add(self, data: dict) -> ModelType:
         stmt = insert(self.model).values(**data).returning(self.model)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def get(self, reference):
+    async def get(self, reference: int) -> ModelType:
         stmt = select(self.model).where(self.model.id == reference)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def get_all(self) -> list[TypeVar]:
+    async def get_all(self) -> list[ModelType]:
         result = await self.session.scalars(select(self.model))
         return result.all()
 
-    async def update(self, reverence, data: dict):
+    async def update(self, reverence: int, data: dict) -> ModelType:
         stmt = update(self.model).where(self.model.id == reverence).values(**data).returning(self.model)
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def exists(self, reference):
+    async def exists(self, reference: int) -> bool:
         stmt = select(exists(self.model).where(self.model.id == reference))
         result = await self.session.execute(stmt)
         return result.scalar()
