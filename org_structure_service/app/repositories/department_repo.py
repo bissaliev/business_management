c71a97df9136284d12dea50bf869036b48714ef7
bissaliev <bissaliev21@gmail.com>
2025-04-11 16:05:16 +0400

    org_structure_service: Удалил метод parent_exists из репозитория DepartmentRepository

--- org_structure_service/app/repositories/department_repo.py
+++ org_structure_service/app/repositories/department_repo.py
@@ -1,16 +1,6 @@
-from sqlalchemy import select
-
 from app.models import Department
 from app.repositories.base_repository import BaseRepository
 
 
 class DepartmentRepository(BaseRepository):
     model = Department
-
-    async def parent_exists(self, parent_id: int, team_id: int):
-        stmt = select(Department).where(
-            Department.id == parent_id,
-            Department.team_id == team_id,
-        )
-        result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
