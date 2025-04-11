f77e603805f4b5c4ce22bc8f270cc53e63b94f93
bissaliev <bissaliev21@gmail.com>
2025-04-11 16:08:23 +0400

    org_structure_service: Добавил модель pydantic DivisionCreate в app/schemas/divisions.py

--- org_structure_service/app/schemas/divisions.py
+++ org_structure_service/app/schemas/divisions.py
@@ -13,10 +13,27 @@ class SDivision(BaseModel):
     model_config = {"from_attributes": True}
 
 
+class DivisionCreate(BaseModel):
+    """Модель для создания дивизии"""
+
+    team_id: int
+    name: str
+
+
 class DivisionResponse(BaseModel):
-    """Модель дивизиона (для дивизионной структуры)"""
+    """Модель-ответ дивизии"""
 
     id: int
+    team_id: int
     name: str
+
+    model_config = {"from_attributes": True}
+
+
+class DivisionUpdate(BaseModel):
+    """Модель для обновления дивизии"""
+
+    team_id: int | None = None
+    name: str | None = None
 
     model_config = {"from_attributes": True}
