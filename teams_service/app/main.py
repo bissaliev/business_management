2e5d8ae81e89eef1b87b464acb6914686f319c4f
bissaliev <bissaliev21@gmail.com>
2025-04-12 18:47:24 +0400

    team_service: Зарегистрировал маршруты app/routers/team_news.py в главный модуль

--- teams_service/app/main.py
+++ teams_service/app/main.py
@@ -4,6 +4,7 @@ from sqladmin import Admin
 from app.admin.views import TeamAdmin, TeamEmployeeAdmin
 from app.database import async_engine
 from app.routers.employees import router as employee_router
+from app.routers.team_news import router as team_news_router
 from app.routers.teams import router as team_router
 
 app = FastAPI()
@@ -15,3 +16,4 @@ admin.add_view(TeamEmployeeAdmin)
 
 app.include_router(team_router, prefix="/teams", tags=["teams"])
 app.include_router(employee_router, prefix="/teams", tags=["employees"])
+app.include_router(team_news_router, prefix="/teams", tags=["team_news"])
