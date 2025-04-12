c9355806740576e71e8a050988980aded6bf2caa
bissaliev <bissaliev21@gmail.com>
2025-04-12 15:58:31 +0400

    team_service: Выделил модели pydantic для работников в отдельный модуль app/schemas/employees.py

--- teams_service/app/schemas/teams.py
+++ teams_service/app/schemas/teams.py
@@ -2,15 +2,17 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
-from app.models.teams import EmployeeRole
-
 
 class TeamCreate(BaseModel):
+    """Создание команды"""
+
     name: str
     description: str
 
 
 class TeamResponse(BaseModel):
+    """Модель ответа команды"""
+
     id: int
     name: str
     description: str
@@ -19,18 +21,8 @@ class TeamResponse(BaseModel):
     model_config = {"from_attributes": True}
 
 
-class AddEmployee(BaseModel):
-    employee_id: int
-    role: EmployeeRole = EmployeeRole.EMPLOYEE
-
-
 class TeamUpdate(BaseModel):
+    """Модель обновления команды"""
+
     name: str | None = None
     description: str | None = None
-
-
-class TeamEmployeeResponse(BaseModel):
-    id: int
-    employee_id: int
-    team_id: int
-    role: EmployeeRole
