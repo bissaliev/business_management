cef69e6434b97b4e062ba77e0805eae323d59498
bissaliev <bissaliev21@gmail.com>
2025-04-12 15:59:27 +0400

    team_service: Добавил в репозиторий TeamRepository метод get_team_by_team_code

--- teams_service/app/repositories/team_repo.py
+++ teams_service/app/repositories/team_repo.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from app.models.teams import Team
 from app.repositories.base_repository import BaseRepository
 
@@ -6,3 +8,9 @@ class TeamRepository(BaseRepository):
     """Репозиторий команд"""
 
     model = Team
+
+    async def get_team_by_team_code(self, team_code: str) -> Team:
+        """Получение команды по team_code"""
+        stmt = select(self.model).where(self.model.team_code == team_code)
+        result = await self.session.scalars(stmt)
+        return result.first()
