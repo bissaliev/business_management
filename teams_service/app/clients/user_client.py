39da4c1720b466c3a5e525702e6a3452e3847e49
bissaliev <bissaliev21@gmail.com>
2025-04-12 16:24:48 +0400

    team_service: Добавил в UserServiceClient метод delete_user для удаления работника

--- teams_service/app/clients/user_client.py
+++ teams_service/app/clients/user_client.py
@@ -6,14 +6,17 @@ from app.schemas.employees import UserResponse
 
 
 class UserServiceClient:
+    """Клиент для запросов на user service"""
+
     def __init__(self, base_url: str = settings.get_user_url()):
         self.base_url = base_url
 
     async def create_user(self, name: str, email: str, password: str) -> UserResponse:
+        """Регистрация пользователя на user service"""
         try:
             async with httpx.AsyncClient(timeout=httpx.Timeout(10.0, connect=5.0)) as client:
                 response = await client.post(
-                    f"{self.base_url}",
+                    f"{self.base_url}/register",
                     json={
                         "email": email,
                         "name": name,
@@ -29,6 +32,21 @@ class UserServiceClient:
                 status_code=500,
                 detail=f"Ошибка при получении пользователя: {e.response.status_code}, {e.response.text}",
             ) from e
-        except httpx.ConnectError:
-            # raise HTTPException(status_code=503, detail="Сервис пользователей не доступен") from e
-            return UserResponse.model_validate({"id": 1, "name": name, "email": email})
+        except httpx.ConnectError as e:
+            raise HTTPException(status_code=503, detail="Сервис пользователей не доступен") from e
+
+    async def delete_user(self, user_id: int):
+        """Удаление пользователя на user service"""
+        try:
+            async with httpx.AsyncClient(timeout=httpx.Timeout(10.0, connect=5.0)) as client:
+                response = await client.delete(f"{self.base_url}/{user_id}")
+                response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 404:
+                raise HTTPException(status_code=404, detail=f"Пользователь с {user_id} не существует") from e
+            raise HTTPException(
+                status_code=500,
+                detail=f"Ошибка при получении пользователя: {e.response.status_code}, {e.response.text}",
+            ) from e
+        except httpx.ConnectError as e:
+            raise HTTPException(status_code=503, detail="Сервис пользователей не доступен") from e
