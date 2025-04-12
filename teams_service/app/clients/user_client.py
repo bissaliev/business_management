a31f631fe40e889dd1dcc64444851ab6961a041e
bissaliev <bissaliev21@gmail.com>
2025-04-12 08:57:21 +0400

    team_service: Добавил UserServiceClient для осуществления запросов на User Service для получения пользователя

---
+++ teams_service/app/clients/user_client.py
@@ -0,0 +1,36 @@
+import httpx
+from fastapi import HTTPException
+
+from app.config import settings
+from app.schemas.employees import UserResponse
+
+
+class UserServiceClient:
+    def __init__(self, base_url: str = settings.get_user_url()):
+        self.base_url = base_url
+
+    async def get_user(self, employee_id: int) -> UserResponse:
+        try:
+            async with httpx.AsyncClient(timeout=httpx.Timeout(10.0, connect=5.0)) as client:
+                response = await client.get(f"{self.base_url}/{employee_id}")
+                response.raise_for_status()
+                return response.json()
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 404:
+                return None
+            raise HTTPException(
+                status_code=502,
+                detail=f"Ошибка при получении пользователя: {e.response.status_code}, {e.response.text}",
+            ) from e
+        except httpx.RequestError:
+            # raise HTTPException(
+            #     status_code=503, detail=f"Сервис пользователей недоступен: {e.__class__.__name__}"
+            # ) from e
+            # TODO: Поставлена заглушка при получении пользователя
+            user_data = {
+                "user_id": employee_id,
+                "name": "Пользователь",
+                "email": "employee@user.com",
+                "status": "сотрудник",
+            }
+            return UserResponse.model_validate(user_data)
