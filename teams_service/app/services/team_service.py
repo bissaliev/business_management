25fd505de6d1eec2ba954d698db03c843b12f0c4
bissaliev <bissaliev21@gmail.com>
2025-04-12 10:07:25 +0400

    team_service: Обновил app/services/team_service.py, вынес логику работы с работниками в отдельный сервис

--- teams_service/app/services/team_service.py
+++ teams_service/app/services/team_service.py
@@ -1,14 +1,14 @@
-import httpx
 from fastapi import HTTPException
-from sqlalchemy import insert, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.config import settings
-from app.models.teams import Team, TeamEmployee
+from app.models.teams import Team
 from app.repositories.team_repo import TeamRepository
 
 
 class TeamService:
+    """Сервис для управления командами"""
+
     def __init__(self, session: AsyncSession):
         self.session = session
         self.repo = TeamRepository(session)
@@ -18,44 +18,25 @@ class TeamService:
         result = await self.session.scalars(stmt)
         return result.first()
 
-    async def get_team_by_id(self, team_id: int):
-        stmt = select(Team).where(Team.team_id == team_id)
-        result = await self.session.scalars(stmt)
-        return result.first()
+    async def _exists_team(self, team_id: int):
+        """Проверка на существование команды"""
+        return await self.repo.exists(team_id)
 
     async def create_team(self, team_data: dict):
-        result = await self.session.scalars(insert(Team).values(**team_data).returning(Team))
-        return result.first()
-
-    async def get_employee(self, user_id: int):
-        async with httpx.AsyncClient() as client:
-            url = f"{settings.USER_HOST}/{settings.USER_PORT}/users/{user_id}"
-            response = await client.get(url)
-            if response.status_code != 200:
-                raise HTTPException(status_code=400, detail=f"Пользователь с таким user_id={user_id} не существует")
-        return response.json()
-
-    async def employee_exists(self, employee_id: int):
-        async with httpx.AsyncClient() as client:
-            url = f"{settings.USER_HOST}/{settings.USER_PORT}/users/{employee_id}"
-            response = await client.get(url)
-            if response.status_code == 200:
-                return True
-        return False
-
-    async def add_employee(self, team_id: int, data: dict):
-        """Добавление сотрудника в команду"""
-        employee_id = data["employee_id"]
-        employee_exists = await self.get_employee(employee_id)
-        if not employee_exists:
-            raise HTTPException(status_code=404, detail=f"Пользователь с таким user_id={employee_id} не существует")
-        team = await self.get_team_by_id(team_id)
-        stmt = insert(TeamEmployee).values(team_id=team.id, **data)
-        await self.session.execute(stmt)
+        """Создание команды"""
+        return await self.repo.create(team_data)
 
     async def get_teams(self) -> list[Team]:
+        """Получение всех команд"""
         teams = await self.repo.get_all()
         return teams
 
     async def get_one(self, team_id: int) -> Team:
+        """Получение одной команды"""
         return await self.repo.get(team_id)
+
+    async def update_team(self, team_id: int, update_data: dict) -> Team:
+        """Обновление данных команды"""
+        if not await self.repo.exists(team_id):
+            raise HTTPException(status_code=404, detail=f"Команды с таким {team_id=} не существует")
+        return await self.repo.update(team_id, update_data)
