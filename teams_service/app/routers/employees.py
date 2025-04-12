855ffbeece678960603aee4d3551d3c12dc0ddd8
bissaliev <bissaliev21@gmail.com>
2025-04-12 16:01:59 +0400

    team_service: Добавил в маршруты регистрация сотрудника и удаление

--- teams_service/app/routers/employees.py
+++ teams_service/app/routers/employees.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
 
 from app.routers.dependencies import TeamEmployeeServiceDeps
-from app.schemas.teams import AddEmployee, EmployeeUpdateRole, TeamEmployeeResponse
+from app.schemas.employees import AddEmployee, EmployeeCreate, EmployeeUpdateRole, TeamEmployeeResponse
 
 router = APIRouter()
+
+
+@router.post("/employees/", summary="Регистрация сотрудника")
+async def create_employee(
+    team_emp_service: TeamEmployeeServiceDeps, employee_data: EmployeeCreate
+) -> TeamEmployeeResponse:
+    return await team_emp_service.create_employee(employee_data.model_dump())
 
 
 @router.get("/{team_id}/employees/", summary="Получение всех сотрудников определенной команды")
@@ -30,3 +37,9 @@ async def update_status(
     team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int, role: EmployeeUpdateRole
 ) -> TeamEmployeeResponse:
     return await team_emp_service.update_role(team_id, employee_id, role.model_dump())
+
+
+@router.delete("/{team_id}/employees/{employee_id}", summary="Удаление сотрудника")
+async def delete_employee(team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int) -> dict[str, str]:
+    await team_emp_service.delete_employee(team_id, employee_id)
+    return {"message": "Работник удален"}
