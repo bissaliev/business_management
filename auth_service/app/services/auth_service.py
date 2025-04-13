838bf21b15817c4fdc8ddcb4c9d091e1aa0e31f0
bissaliev <bissaliev21@gmail.com>
2025-04-13 16:06:39 +0400

    auth_service: Добавил сервис для управления аутентификацией

---
+++ auth_service/app/services/auth_service.py
@@ -0,0 +1,76 @@
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth import create_access_token, decode_access_token, get_password_hash, verify_password
+from app.clients.team_client import TeamServiceClient
+from app.models.users import Status, User
+from app.repositories.user_repo import UserRepository
+from app.schemas.users import Token
+
+
+class AuthService:
+    """Сервис для управления пользователями"""
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.repo = UserRepository(session)
+        self.team_client = TeamServiceClient()
+
+    async def create(self, user_data: dict):
+        """Создание пользователя"""
+        if await self.repo.get_user_by_email(user_data["email"]):
+            raise HTTPException(status_code=409, detail="Пользователь с таким email существует")
+
+        password = user_data.pop("password")
+        hashed_password = get_password_hash(password)
+        user_data["hashed_password"] = hashed_password
+        return await self.repo.create(user_data)
+
+    async def authenticate_user(self, email: str, password: str):
+        user = await self.repo.get_user_by_email(email)
+        if not user:
+            return False
+        if not verify_password(password, user.hashed_password):
+            return False
+        return user
+
+    async def login(self, email: str, password: str) -> Token:
+        user = await self.authenticate_user(email, password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        if not user.is_active:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user")
+        data = {"sub": user.email, "id": user.id, "status": user.status.value}
+        role = await self.team_client.get_employees_role(user.id)
+        data |= role
+        access_token = create_access_token(data=data)
+        return Token(access_token=access_token, token_type="bearer")
+
+    async def get_current_user(self, token: str) -> User:
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        payload = decode_access_token(token)
+        if payload is None:
+            raise credentials_exception
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        user = await self.repo.get_user_by_email(email)
+        if user is None or not user.is_active:
+            raise credentials_exception
+        return user
+
+    async def get_current_admin(self, token: str) -> User:
+        user = await self.get_current_user(token)
+        if user.status != Status.ADMIN:
+            raise HTTPException(status_code=403, detail="Not enough permissions")
+        return user
+
+    async def verify_token(): ...
