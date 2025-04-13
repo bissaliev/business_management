36a221ce55fbf47aaa5b79de94ce359bbe76222c
bissaliev <bissaliev21@gmail.com>
2025-04-13 16:07:07 +0400

    auth_service: Перенес создание пользователя в сервис аутентификации

--- auth_service/app/services/user_service.py
+++ auth_service/app/services/user_service.py
@@ -1,7 +1,6 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.auth import get_password_hash
 from app.models.users import User
 from app.repositories.user_repo import UserRepository
 
@@ -16,16 +15,6 @@ class UserService:
     async def get_users(self) -> list[User]:
         """Получение всех пользователей"""
         return await self.repo.get_all()
-
-    async def create(self, user_data: dict):
-        """Создание пользователя"""
-        if await self.repo.get_user_by_email(user_data["email"]):
-            raise HTTPException(status_code=409, detail="Пользователь с таким email существует")
-
-        password = user_data.pop("password")
-        hashed_password = get_password_hash(password)
-        user_data["hashed_password"] = hashed_password
-        return await self.repo.create(user_data)
 
     async def get_user(self, id: int) -> User:
         """Получение одного пользователя"""
