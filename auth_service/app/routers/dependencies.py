04ec5d03a92e30e47ac555151935db1596a76656
bissaliev <bissaliev21@gmail.com>
2025-04-13 16:05:47 +0400

    auth_service: Добавил в зависимости CurrentUserDeps

--- auth_service/app/routers/dependencies.py
+++ auth_service/app/routers/dependencies.py
@@ -3,7 +3,11 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.auth import oauth2_scheme
+from app.clients.team_client import TeamServiceClient
 from app.database import get_session
+from app.models.users import User
+from app.services.auth_service import AuthService
 from app.services.user_service import UserService
 
 
@@ -12,3 +16,28 @@ async def user_service(session: Annotated[AsyncSession, Depends(get_session)]):
 
 
 UserServiceDeps = Annotated[UserService, Depends(user_service)]
+
+
+async def team_service_client():
+    return TeamServiceClient()
+
+
+TeamServiceClientDeps = Annotated[TeamServiceClient, Depends(team_service_client)]
+
+
+async def auth_service(session: Annotated[AsyncSession, Depends(get_session)]):
+    return AuthService(session)
+
+
+AuthServiceDeps = Annotated[AuthService, Depends(auth_service)]
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], session: Annotated[AsyncSession, Depends(get_session)]
+):
+    auth_service = AuthService(session)
+    user = await auth_service.get_current_user(token)
+    return user
+
+
+CurrentUserDeps = Annotated[User, Depends(get_current_user)]
