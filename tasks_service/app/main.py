e5c745c09b02cf24a4c3621dc64f2a0b013b7062
bissaliev <bissaliev21@gmail.com>
2025-04-14 16:42:56 +0400

    tasks_service: Зарегистрировал маршруты для управления комментариев в главном модуле

--- tasks_service/app/main.py
+++ tasks_service/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from app.routers.comments import router as comment_router
 from app.routers.tasks import router as task_router
 
 app = FastAPI(root_path="/tasks-service")
 
 
 app.include_router(task_router, prefix="/tasks", tags=["tasks"])
+app.include_router(comment_router, prefix="/tasks", tags=["comments"])
