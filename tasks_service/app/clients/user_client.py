667c0775e0112397ab294ae74c87286dec7cc5de
bissaliev <bissaliev21@gmail.com>
2025-04-14 11:06:14 +0400

    tasks_service: Добавил class UserServiceClient:  для запросов на сервис пользователей

---
+++ tasks_service/app/clients/user_client.py
@@ -0,0 +1,26 @@
+# task_service/clients/user_service_client.py
+import httpx
+from fastapi import HTTPException
+
+from app.config import settings
+
+BASE_URL1 = settings.get_user_url()
+BASE_URL = "http://localhost:8001/users"
+
+
+class UserServiceClient:
+    def __init__(self, base_url: str = BASE_URL):
+        self.base_url = base_url
+
+    async def verify_token(self, token: str) -> dict:
+        try:
+            async with httpx.AsyncClient(timeout=httpx.Timeout(10.0, connect=5.0)) as client:
+                response = await client.get(
+                    f"{self.base_url}/auth/verify", headers={"Authorization": f"Bearer {token}"}
+                )
+                response.raise_for_status()
+                return response.json()
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code=e.response.status_code, detail="Invalid token or user") from e
+        except httpx.ConnectError as e:
+            raise HTTPException(status_code=503, detail="Сервис User service не доступен") from e
