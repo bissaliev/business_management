78f63771015d50a0174294d1543d55109866d9c6
bissaliev <bissaliev21@gmail.com>
2025-04-14 10:55:25 +0400

    tasks_service: Добавил модель Comment

---
+++ tasks_service/app/models/comments.py
@@ -0,0 +1,17 @@
+# task_service/models/comment.py
+from datetime import datetime, timezone
+
+from sqlalchemy import ForeignKey, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
+    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id", ondelete="CASCADE"), nullable=False)
+    user_id: Mapped[int]  # Автор комментария
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
