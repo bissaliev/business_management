dcfc7bf9e945516541a7ef7c40f267680e6bff91
bissaliev <bissaliev21@gmail.com>
2025-04-15 08:18:08 +0400

    org_structure_service: Добавил метод ы в репозиторий EmployeeRepository (get_employees_by_department_id, get_employee, update_employee, delete_employee)

--- org_structure_service/app/repositories/employee_repo.py
+++ org_structure_service/app/repositories/employee_repo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import exists, select
+from sqlalchemy import delete, exists, select, update
 
 from app.models import EmployeeStructure
 from app.repositories.base_repository import BaseRepository
@@ -15,8 +15,33 @@ class EmployeeRepository(BaseRepository):
         result = await self.session.execute(stmt)
         return result.scalar()
 
-    async def get_department_employees(self, department_id: int) -> list[EmployeeStructure]:
+    async def get_employees_by_department_id(self, department_id: int) -> list[EmployeeStructure]:
         """Получить список сотрудников по идентификатору департамента"""
         stmt = select(self.model).where(self.model.department_id == department_id)
         result = await self.session.scalars(stmt)
         return result.all()
+
+    async def get_employee(self, employee_id: int):
+        """Получить сотрудника"""
+        stmt = select(self.model).where(self.model.employee_id == employee_id)
+        return (await self.session.scalars(stmt)).first()
+
+    async def update_employee(self, employee_id: int, **data: dict) -> EmployeeStructure:
+        stmt = update(self.model).where(self.model.employee_id == employee_id).values(**data).returning(self.model)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
+    async def delete_employee(self, employee_id: int) -> bool:
+        stmt = delete(self.model).where(self.model.employee_id == employee_id)
+        result = await self.session.execute(stmt)
+        return result.rowcount > 0
+
+    async def add_manager(self, department_id: int, manager_id):
+        """Получить сотрудника"""
+        stmt = (
+            update(self.model)
+            .where(self.model.department_id == department_id)
+            .values(manager_id=manager_id)
+            .returning(self.model)
+        )
+        return (await self.session.scalars(stmt)).all()
