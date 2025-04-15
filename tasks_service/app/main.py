cefb375f531b3482bea498fd75b302683d00ba20
bissaliev <bissaliev21@gmail.com>
2025-04-15 11:34:41 +0400

    tasks_service: Подключил маршруты оценки задач в главный модуль

--- tasks_service/app/main.py
+++ tasks_service/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.routers.comments import router as comment_router
+from app.routers.task_evaluation import router as task_evaluation_router
 from app.routers.tasks import router as task_router
 
 app = FastAPI(root_path="/tasks-service")
@@ -8,3 +9,4 @@ app = FastAPI(root_path="/tasks-service")
 
 app.include_router(task_router, prefix="/tasks", tags=["tasks"])
 app.include_router(comment_router, prefix="/tasks", tags=["comments"])
+app.include_router(task_evaluation_router, prefix="/tasks", tags=["task evaluation"])
