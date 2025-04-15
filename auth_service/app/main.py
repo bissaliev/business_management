766d8664b7f1927b1cbe4ab851dd496b153d4ff3
bissaliev <bissaliev21@gmail.com>
2025-04-15 13:01:02 +0400

    auth_service: Включил миддлвейр CORSMiddleware

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqladmin import Admin
 
 from app.admin.authentication import authentication_backend
@@ -13,6 +14,14 @@ admin = Admin(app, async_engine, authentication_backend=authentication_backend)
 
 
 admin.add_view(UserAdmin)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Укажите ваши домены
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(user_router, prefix="/users", tags=["users"])
