3500dc906e9a4662fe27cd37416f81ab039aefee
bissaliev <bissaliev21@gmail.com>
2025-04-16 22:00:38 +0400

    calendar_service: Добавил маршруты для календаря за месяц и за один день

---
+++ calendar_service/app/routers/calendar.py
@@ -0,0 +1,22 @@
+from datetime import date
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from app.routers.dependencies import CalendarServiceDeps, CurrentUser
+from app.schemas.calendar import CalendarDayResponse, CalendarMonthResponse, YearMonthParams
+
+router = APIRouter()
+
+
+@router.get("/month", response_model=list[CalendarMonthResponse])
+async def get_monthly_calendar(
+    params: Annotated[YearMonthParams, Depends()], user: CurrentUser, calendar_service: CalendarServiceDeps
+):
+    calendar = await calendar_service.get_monthly_events(params, user)
+    return calendar
+
+
+@router.get("/day", response_model=CalendarDayResponse)
+async def get_daily_calendar(date: date, user: CurrentUser, calendar_service: CalendarServiceDeps):
+    return await calendar_service.get_daily_events(date, user)
