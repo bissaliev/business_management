0591bf2c269a5ebd471e4a8672bf0487ea3f286b
bissaliev <bissaliev21@gmail.com>
2025-04-16 07:30:37 +0400

    meeting_service: Добавил репозиторий MeetingRepository

--- meetings_service/app/repositories/meeting_repo.py
+++ meetings_service/app/repositories/meeting_repo.py
@@ -1,23 +1,48 @@
-from sqlalchemy import literal, select
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import and_, or_, select, update
+from sqlalchemy.orm import selectinload
 
-from app.models.meeting import Meeting
+from app.models.meeting import Meeting, MeetingParticipant
 from app.repositories.base_repository import BaseRepository
 
 
 class MeetingRepository(BaseRepository):
-    """Репозиторий для пользователей"""
+    """Репозиторий для встреч"""
 
     model = Meeting
 
     async def list_meetings(self, employee_id: int, team_id: int | None = None) -> list[Meeting]:
-        stmt = select(self.model)
+        """Получение списка встреч с фильтрацией по идентификатору команды и идентификатору работника"""
+        stmt = (
+            select(self.model)
+            .join(MeetingParticipant, self.model.id == MeetingParticipant.meeting_id)
+            .options(selectinload(self.model.participants))
+        )
         if team_id:
             stmt = stmt.where(self.model.team_id == team_id)
         else:
             stmt = stmt.where(
-                Meeting.participants.contains(literal([employee_id], type_=JSONB))
-                | (self.model.creator_id == employee_id)
+                (self.model.creator_id == employee_id) | (MeetingParticipant.participant_id == employee_id)
             )
         meetings = (await self.session.execute(stmt)).scalars().all()
         return meetings
+
+    async def get_meeting(self, employee_id: int, meeting_id: int) -> Meeting:
+        """Получение объект встреч по id команды и идентификатору работника"""
+        stmt = (
+            select(self.model)
+            .join(MeetingParticipant, self.model.id == MeetingParticipant.meeting_id)
+            .options(selectinload(self.model.participants))
+            .where(
+                and_(
+                    self.model.id == meeting_id,
+                    or_(self.model.creator_id == employee_id, MeetingParticipant.participant_id == employee_id),
+                )
+            )
+        )
+        meeting = (await self.session.execute(stmt)).scalars().first()
+        return meeting
+
+    async def update_meeting(self, meeting_id: int, **update_data: dict) -> Meeting:
+        """Обновление встречи"""
+        stmt = update(self.model).where(self.model.id == meeting_id).values(**update_data).returning(self.model)
+        return (await self.session.scalars(stmt)).first()
