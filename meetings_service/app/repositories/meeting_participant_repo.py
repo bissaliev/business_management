3a275ad83122f842e756d481dacac88caf8b8d63
bissaliev <bissaliev21@gmail.com>
2025-04-17 15:10:43 +0400

    meeting_service: Добавил получения id-ков участников встречи

--- meetings_service/app/repositories/meeting_participant_repo.py
+++ meetings_service/app/repositories/meeting_participant_repo.py
@@ -10,12 +10,19 @@ class MeetingParticipantRepository(BaseRepository):
     model = MeetingParticipant
 
     async def exists_participant(self, meeting_id: int, participant_id: int) -> bool:
+        """Проверка на существование участника во встрече"""
         stmt = select(exists().where(self.model.meeting_id == meeting_id, self.model.participant_id == participant_id))
         return await self.session.scalar(stmt)
 
     async def delete_participant(self, meeting_id: int, participant_id: int) -> bool:
+        """Удаление участника из встречи"""
         stmt = delete(self.model).where(
             self.model.meeting_id == meeting_id, self.model.participant_id == participant_id
         )
         result = await self.session.execute(stmt)
         return result.rowcount > 0
+
+    async def get_participant_ids(self, meeting_id: int) -> list[int]:
+        """Получение идентификаторов участников встречи"""
+        stmt = select(self.model.participant_id).where(self.model.meeting_id == meeting_id)
+        return (await self.session.scalars(stmt)).all()
