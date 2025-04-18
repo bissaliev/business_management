fdc52583c82095af6b8c9e4ad5f7d323decf13b9
bissaliev <bissaliev21@gmail.com>
2025-04-18 17:01:42 +0400

    tasks_service: Изменение в передаче параметров тела запроса в сервис(Вместо словаря отдается модель pydantic)

--- tasks_service/app/routers/comments.py
+++ tasks_service/app/routers/comments.py
@@ -11,7 +11,7 @@ router = APIRouter()
 async def add_comment(
     task_id: int, comment_data: CommentCreate, user: CurrentUser, comment_service: CommentServiceDeps
 ) -> CommentResponse:
-    return await comment_service.create_comment(task_id, user, comment_data.model_dump())
+    return await comment_service.create_comment(task_id, user, comment_data)
 
 
 @router.get("/{task_id}/comments", dependencies=[TaskMemberPermission], summary="Получение комментариев задачи")
@@ -25,12 +25,12 @@ async def get_comments(task_id: int, comment_service: CommentServiceDeps) -> lis
 async def update_comment(
     task_id: int, comment_id: int, comment_data: CommentCreate, user: CurrentUser, comment_service: CommentServiceDeps
 ) -> CommentResponse:
-    return await comment_service.update_comment(task_id, comment_id, user, comment_data.model_dump(exclude_unset=True))
+    return await comment_service.update_comment(task_id, comment_id, user, comment_data)
 
 
 @router.delete("/{task_id}/comments{comment_id}", dependencies=[TaskMemberPermission], summary="Удаление комментария")
 async def delete_comment(
     task_id: int, comment_id: int, user: CurrentUser, comment_service: CommentServiceDeps
 ) -> MessageDelete:
-    await comment_service.delete_comment(task_id, comment_id)
+    await comment_service.delete_comment(task_id, user, comment_id)
     return {"message": "Комментарий удален"}
