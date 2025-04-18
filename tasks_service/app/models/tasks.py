f161ddd32e5104bac386cdae13ecfcc54d2013fe
bissaliev <bissaliev21@gmail.com>
2025-04-18 07:13:43 +0400

    tasks_service: Добавил поля start_time и end_time в модель Task

--- tasks_service/app/models/tasks.py
+++ tasks_service/app/models/tasks.py
@@ -1,7 +1,7 @@
 import enum
 from datetime import datetime
 
-from sqlalchemy import Enum, String, Text
+from sqlalchemy import DateTime, Enum, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -30,12 +30,14 @@ class Task(Base):
     title: Mapped[str] = mapped_column(String(250), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
     status: Mapped[TaskStatus] = mapped_column(Enum(TaskStatus, native_enum=False), default=TaskStatus.OPEN)
+    start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
-    due_date: Mapped[datetime | None] = mapped_column(nullable=True)  # Дедлайн
     creator_id: Mapped[int]  # Создатель (руководитель)
     assignee_id: Mapped[int]  # Исполнитель (подчинённый)
     team_id: Mapped[int]  # Команда
+    # TODO: Добавить период выполнения
 
     def __repr__(self):
         return f"Task(id={self.id}, title={self.title}, status={self.status})"
