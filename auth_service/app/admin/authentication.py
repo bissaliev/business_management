7ec916cbc16b93fc35a434da5ea28fbfc8262c9e
bissaliev <bissaliev21@gmail.com>
2025-04-18 11:37:39 +0400

    auth_service: Настроил аутентификацию в админке с использованием сервиса AuthService

--- auth_service/app/admin/authentication.py
+++ auth_service/app/admin/authentication.py
@@ -1,26 +1,23 @@
-from jose import JWTError, jwt
 from sqladmin.authentication import AuthenticationBackend
 from starlette.requests import Request
 
 from app.config import settings
 from app.database import SessionLocal
-from app.security import create_access_token, verify_password
-from app.services.user_service import UserService
+from app.services.auth_service import AuthService
 
 
 class AdminAuth(AuthenticationBackend):
+    """Аутентификация в админ панели"""
+
+    auth_service = AuthService
+
     async def login(self, request: Request) -> bool:
         form = await request.form()
         email, password = form["username"], form["password"]
         async with SessionLocal() as session:
-            user_service = UserService(session)
-            user = await user_service.get_user_by_email(email)
-        if not user:
-            return False
-        if not verify_password(password, user.hashed_password):
-            return False
-        access_token = create_access_token({"sub": str(user.id)})
-        request.session.update({"token": str(access_token)})
+            service = self.auth_service(session)
+            token = await service.login(email, password)
+        request.session.update({"token": str(token.access_token)})
         return True
 
     async def logout(self, request: Request) -> bool:
@@ -33,13 +30,11 @@ class AdminAuth(AuthenticationBackend):
         if not token:
             return False
 
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-            user_id: int = payload.get("sub")
-            if user_id is None:
+        async with SessionLocal() as session:
+            service = self.auth_service(session)
+            user = await service.get_current_admin(token)
+            if user is None:
                 return False
-        except JWTError:
-            return False
         return True
 
 
