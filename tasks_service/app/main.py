854fe5c579ff2bd892b53c0eee434e3027fe3bc2
bissaliev <bissaliev21@gmail.com>
2025-04-18 15:57:12 +0400

    auth_service: Подключил админ-панель к приложению в главном меню

--- tasks_service/app/main.py
+++ tasks_service/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from sqladmin import Admin
 
+from app.admin.authentication import authentication_backend
+from app.admin.views import TaskAdmin, TaskEvaluationAdmin
+from app.database import SessionLocal
 from app.routers.comments import router as comment_router
 from app.routers.task_evaluation import router as task_evaluation_router
 from app.routers.tasks import router as task_router
@@ -10,3 +14,7 @@ app = FastAPI(root_path="/tasks-service")
 app.include_router(task_router, prefix="/tasks", tags=["tasks"])
 app.include_router(comment_router, prefix="/tasks", tags=["comments"])
 app.include_router(task_evaluation_router, prefix="/tasks", tags=["task evaluation"])
+
+admin = Admin(app, session_maker=SessionLocal, authentication_backend=authentication_backend)
+admin.add_view(TaskAdmin)
+admin.add_view(TaskEvaluationAdmin)
