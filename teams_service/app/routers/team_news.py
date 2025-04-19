2ca6923dc66f531db0a6cedb4c9e8f8243ca52aa
bissaliev <bissaliev21@gmail.com>
2025-04-19 08:19:47 +0400

    teams_service: Изменил передачи словаря на передачу модели pydantic в сервис TeamNewsService

--- teams_service/app/routers/team_news.py
+++ teams_service/app/routers/team_news.py
@@ -15,7 +15,7 @@ async def get_team_news(team_id: int, team_news_service: TeamNewsServiceDeps) ->
 async def create_team(
     team_id: int, team_news_service: TeamNewsServiceDeps, news_data: TeamNewsCreate
 ) -> TeamNewsResponse:
-    return await team_news_service.create_news(team_id, news_data.model_dump())
+    return await team_news_service.create_news(team_id, news_data)
 
 
 @router.get("/{team_id}/news/{id}", summary="Получение одной новости команды по id")
@@ -27,7 +27,7 @@ async def get_team_news_item(team_id: int, id: int, team_news_service: TeamNewsS
 async def update_team_news(
     team_news_service: TeamNewsServiceDeps, team_id: int, id: int, update_data: TeamNewsUpdate
 ) -> TeamNewsResponse:
-    return await team_news_service.update_news(team_id, id, update_data.model_dump(exclude_unset=True))
+    return await team_news_service.update_news(team_id, id, update_data)
 
 
 @router.delete("/{team_id}/news{id}", summary="Удаление данных новости команды")
