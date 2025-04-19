31a452c70581de6b162f136762084b6a8599404d
bissaliev <bissaliev21@gmail.com>
2025-04-19 07:46:03 +0400

    teams_service: Добавил маршрут удаления команды в app/routers/teams.py

--- teams_service/app/routers/teams.py
+++ teams_service/app/routers/teams.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
 from app.routers.dependencies import TeamServiceDeps
+from app.schemas.responses import MessageDelete
 from app.schemas.teams import TeamCreate, TeamResponse, TeamUpdate
 
 router = APIRouter()
@@ -14,7 +15,7 @@ async def get_teams(team_service: TeamServiceDeps) -> list[TeamResponse]:
 
 @router.post("/", summary="Регистрация команды")
 async def create_team(team_service: TeamServiceDeps, team_data: TeamCreate) -> TeamResponse:
-    team = await team_service.create_team(team_data.model_dump())
+    team = await team_service.create_team(team_data)
     return team
 
 
@@ -23,14 +24,20 @@ async def get_team(id: int, team_service: TeamServiceDeps) -> TeamResponse:
     return await team_service.get_one(id)
 
 
+@router.put("/{team_id}", summary="Обновление данных команды")
+async def update_team(team_service: TeamServiceDeps, team_id: int, update_data: TeamUpdate) -> TeamResponse:
+    return await team_service.update_team(team_id, update_data)
+
+
+@router.delete("/{team_id}", summary="Обновление данных команды")
+async def delete_team(team_service: TeamServiceDeps, team_id: int) -> MessageDelete:
+    await team_service.delete_team(team_id)
+    return MessageDelete()
+
+
 @router.get("/by-code/{team_code}", summary="Получение команды по team_code")
 async def get_team_by_code(team_service: TeamServiceDeps, team_code: str) -> TeamResponse:
     team = await team_service.get_team_by_code(team_code)
     if not team:
         raise HTTPException(status_code=404, detail="Team not found")
     return team
-
-
-@router.put("/{team_id}", summary="Обновление данных команды")
-async def update_team(team_service: TeamServiceDeps, team_id: int, update_data: TeamUpdate) -> TeamResponse:
-    return await team_service.update_team(team_id, update_data.model_dump(exclude_unset=True))
