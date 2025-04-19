a66905fa5a07d9793d877b61885670c3421c185b
bissaliev <bissaliev21@gmail.com>
2025-04-19 11:32:10 +0400

    teams_service: Добавил классы представления и аутентификацию для админ-панели в главный модуль app/main.py

--- teams_service/app/main.py
+++ teams_service/app/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 from sqladmin import Admin
 
-from app.admin.views import TeamAdmin, TeamEmployeeAdmin
+from app.admin.authentication import authentication_backend
+from app.admin.views import TeamAdmin, TeamEmployeeAdmin, TeamNewsAdmin
 from app.database import async_engine
 from app.routers.employees import router as employee_router
 from app.routers.team_news import router as team_news_router
 from app.routers.teams import router as team_router
 
 app = FastAPI(root_path="/teams")
-admin = Admin(app, async_engine)
+admin = Admin(app, async_engine, authentication_backend=authentication_backend)
 
 admin.add_view(TeamAdmin)
 admin.add_view(TeamEmployeeAdmin)
+admin.add_view(TeamNewsAdmin)
 
 
 app.include_router(team_router, prefix="/teams", tags=["teams"])
