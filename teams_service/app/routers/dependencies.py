43d47c0bb61962b4267a3ab62ee7dad672dff8a2
bissaliev <bissaliev21@gmail.com>
2025-04-19 10:40:47 +0400

    teams_service: Добавил в app/routers/dependencies.py зависимости аутентификации

--- teams_service/app/routers/dependencies.py
+++ teams_service/app/routers/dependencies.py
@@ -1,12 +1,18 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.clients.user_client import AuthClient
+from app.config import settings
 from app.database import get_session
+from app.schemas.users import EmployeeRole, User
 from app.services.employee_service import TeamEmployeeService
 from app.services.team_news_service import TeamNewsService
 from app.services.team_service import TeamService
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=settings.URL_TOKEN)
 
 
 def team_service(session: Annotated[AsyncSession, Depends(get_session)]):
@@ -28,3 +34,30 @@ def team_news_service(session: Annotated[AsyncSession, Depends(get_session)]):
 
 
 TeamNewsServiceDeps = Annotated[TeamNewsService, Depends(team_news_service)]
+
+AuthClientDeps = Annotated[AuthClient, Depends(lambda: AuthClient())]
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], user_client: AuthClientDeps) -> User:
+    """Проверяет токен через User Service, возвращает данные (id, email, status, team_role)."""
+    user_data = await user_client.verify_token(token)
+    if not user_data["is_active"]:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Inactive user")
+    return User.model_validate(user_data)
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+async def require_admin(team_id: int, user: Annotated[User, Depends(get_current_user)]) -> User:
+    """
+    Разрешает действия только админам руководителям команды (role="менеджер", "админ")"""
+    if not (user.team_id and user.team_id == team_id):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Не достаточно прав")
+    if user.role != EmployeeRole.ADMINISTRATOR:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Не достаточно прав")
+    return user
+
+
+Admin = Annotated[User, Depends(require_admin)]
+AdminDeps = Depends(require_admin)
