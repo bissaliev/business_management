2bd53ec119dd30079098c2f711a729835836e1f9
bissaliev <bissaliev21@gmail.com>
2025-04-19 10:43:10 +0400

    teams_service: Добавил в маршруты app/routers/teams.py ограничения уровень роли(Администратор)

--- teams_service/app/routers/teams.py
+++ teams_service/app/routers/teams.py
@@ -1,35 +1,23 @@
 from fastapi import APIRouter, HTTPException
 
-from app.routers.dependencies import TeamServiceDeps
+from app.routers.dependencies import AdminDeps, TeamServiceDeps
 from app.schemas.responses import MessageDelete
-from app.schemas.teams import TeamCreate, TeamResponse, TeamUpdate
+from app.schemas.teams import TeamResponse, TeamUpdate
 
 router = APIRouter()
 
 
-@router.get("/", summary="Получение списка команд")
-async def get_teams(team_service: TeamServiceDeps) -> list[TeamResponse]:
-    teams = await team_service.get_teams()
-    return teams
+@router.get("/{team_id}", dependencies=[AdminDeps], summary="Получение команды по id")
+async def get_team(team_id: int, team_service: TeamServiceDeps) -> TeamResponse:
+    return await team_service.get_one(team_id)
 
 
-@router.post("/", summary="Регистрация команды")
-async def create_team(team_service: TeamServiceDeps, team_data: TeamCreate) -> TeamResponse:
-    team = await team_service.create_team(team_data)
-    return team
-
-
-@router.get("/{id}", summary="Получение команды по id")
-async def get_team(id: int, team_service: TeamServiceDeps) -> TeamResponse:
-    return await team_service.get_one(id)
-
-
-@router.put("/{team_id}", summary="Обновление данных команды")
+@router.put("/{team_id}", dependencies=[AdminDeps], summary="Обновление данных команды")
 async def update_team(team_service: TeamServiceDeps, team_id: int, update_data: TeamUpdate) -> TeamResponse:
     return await team_service.update_team(team_id, update_data)
 
 
-@router.delete("/{team_id}", summary="Обновление данных команды")
+@router.delete("/{team_id}", dependencies=[AdminDeps], summary="Удаление команды")
 async def delete_team(team_service: TeamServiceDeps, team_id: int) -> MessageDelete:
     await team_service.delete_team(team_id)
     return MessageDelete()
