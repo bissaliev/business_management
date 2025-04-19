e4020b4970e1b2c2dad164a9ae87c6fc9816c815
bissaliev <bissaliev21@gmail.com>
2025-04-19 10:43:34 +0400

    teams_service: Добавил в маршруты app/routers/employees.py ограничения уровень роли(Администратор)

--- teams_service/app/routers/employees.py
+++ teams_service/app/routers/employees.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter
 
-from app.routers.dependencies import TeamEmployeeServiceDeps
+from app.routers.dependencies import AdminDeps, TeamEmployeeServiceDeps
 from app.schemas.employees import EmployeeCreate, EmployeeUpdateRole, TeamEmployeeResponse
 
 router = APIRouter()
 
 
-@router.post("/employees/", summary="Регистрация сотрудника")
+@router.post("/{team_id}/employees/", dependencies=[AdminDeps], summary="Регистрация сотрудника")
 async def create_employee(
-    team_emp_service: TeamEmployeeServiceDeps, employee_data: EmployeeCreate
+    team_id: int, team_emp_service: TeamEmployeeServiceDeps, employee_data: EmployeeCreate
 ) -> TeamEmployeeResponse:
-    return await team_emp_service.create_employee(employee_data)
+    return await team_emp_service.create_employee(team_id, employee_data)
 
 
-@router.get("/{team_id}/employees/", summary="Получение всех сотрудников определенной команды")
+@router.get(
+    "/{team_id}/employees/", dependencies=[AdminDeps], summary="Получение всех сотрудников определенной команды"
+)
 async def get_team_employees(team_emp_service: TeamEmployeeServiceDeps, team_id: int) -> list[TeamEmployeeResponse]:
     return await team_emp_service.get_team_employees(team_id)
 
@@ -25,14 +27,14 @@ async def get_specific_team_employee(
     return await team_emp_service.get_specific_team_employee(team_id, employee_id)
 
 
-@router.patch("/{team_id}/employees/{employee_id}", summary="Обновление роли работника")
+@router.patch("/{team_id}/employees/{employee_id}", dependencies=[AdminDeps], summary="Обновление роли работника")
 async def update_status(
     team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int, role: EmployeeUpdateRole
 ) -> TeamEmployeeResponse:
     return await team_emp_service.update_role(team_id, employee_id, role)
 
 
-@router.delete("/{team_id}/employees/{employee_id}", summary="Удаление сотрудника")
+@router.delete("/{team_id}/employees/{employee_id}", dependencies=[AdminDeps], summary="Удаление сотрудника")
 async def delete_employee(team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int) -> dict[str, str]:
     await team_emp_service.delete_employee(team_id, employee_id)
     return {"message": "Работник удален"}
