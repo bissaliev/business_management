fe15c4cb24474a54a0b3165788f4d3d6e34ff180
bissaliev <bissaliev21@gmail.com>
2025-04-19 17:34:40 +0400

    org_structure_service: Рефакторинг(Добавил читаемые статус коды и улучшил структуру в app/services/structure_services.py)

--- org_structure_service/app/services/structure_services.py
+++ org_structure_service/app/services/structure_services.py
@@ -1,9 +1,10 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.clients.team_client import TeamServiceClient
+from app.models import TeamStructure
 from app.repositories.team_structure_repo import TeamStructureRepository
-from app.schemas.team_structures import TeamStructureResponse
+from app.schemas.team_structures import TeamStructureCreate, TeamStructureResponse
 
 
 class OrgStructureService:
@@ -14,26 +15,25 @@ class OrgStructureService:
         self.repo = TeamStructureRepository(session)
         self.team_client = TeamServiceClient()
 
-    async def get_team_structure_all(self):
+    async def get_team_structure_all(self) -> list[TeamStructure]:
+        """Получение всех зарегистрированных организационных структур"""
         team_structures = await self.repo.get_all()
         return team_structures
 
-    # TODO: Поправить правильную обработку запросов httpx.AsyncClient
-    async def get_team_on_team_service(self, team_id: int):
-        """Проверка на существование команды в team_service"""
-        team = await self.team_client.get_team(team_id)
-        return team
-
-    # TODO: Подумать о том чтобы создавать команду на этом сервисе в момент создания в team service
-    async def create_team_structure(self, structure_data: dict):
+    async def create_team_structure(self, structure_data: TeamStructureCreate) -> TeamStructure:
         """Создание организационной структуры команды"""
-        team_id = structure_data["team_id"]
-        team = await self.get_team_on_team_service(team_id)
+        team = await self._get_team_on_team_service(structure_data.team_id)
         if team is None:
-            raise HTTPException(status_code=404, detail=f"Команда с id={team_id} еще не зарегистрирована")
-        if await self.repo.exists(team_id):
-            raise HTTPException(status_code=400, detail=f"Структура команды с team_id={team_id} уже существует")
-        team_structure = await self.repo.add(structure_data)
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Команда с id={structure_data.team_id} еще не зарегистрирована",
+            )
+        if await self.repo.exists(structure_data.team_id):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Структура команды с team_id={structure_data.team_id} уже существует",
+            )
+        team_structure = await self.repo.add(**structure_data.model_dump())
         return team_structure
 
     async def get_team_structure(self, team_id: int) -> TeamStructureResponse:
@@ -41,6 +41,13 @@ class OrgStructureService:
         team_structure = await self.repo.get_with_relationship_upload(team_id)
 
         if not team_structure:
-            raise HTTPException(status_code=400, detail=f"Структура команды с team_id={team_id} не существует")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail=f"Структура команды с team_id={team_id} не существует"
+            )
 
         return TeamStructureResponse.model_validate(team_structure)
+
+    async def _get_team_on_team_service(self, team_id: int) -> TeamStructure:
+        """Проверка на существование команды в team_service"""
+        team = await self.team_client.get_team(team_id)
+        return team
