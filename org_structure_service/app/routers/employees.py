06f318c8a1a6460b50ea2215270fe8654cfc1087
bissaliev <bissaliev21@gmail.com>
2025-04-20 10:56:14 +0400

    org_structure_service: Добавил в маршруты app/routers/employees.py параметр department_id

--- org_structure_service/app/routers/employees.py
+++ org_structure_service/app/routers/employees.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.routers.dependencies import EmployeeManagerServiceDeps, EmployeeServiceDeps
+from app.routers.dependencies import EmployeeServiceDeps, OnlyAdminDeps
 from app.schemas.employees import (
     AddManager,
     EmployeeManagerCreate,
@@ -13,53 +13,55 @@ from app.schemas.employees import (
 router = APIRouter()
 
 
-@router.get("/", summary="Список всех сотрудников с фильтрацией по департаменту")
-async def get_employees(
-    employee_service: EmployeeServiceDeps, department_id: int | None = None
-) -> list[EmployeeResponse]:
+@router.get("/{department_id}/employees", summary="Список всех сотрудников департамента")
+async def get_employees(department_id: int, employee_service: EmployeeServiceDeps) -> list[EmployeeResponse]:
     employees = await employee_service.get_employees(department_id)
     return employees
 
 
-@router.post("/", summary="Добавление сотрудника")
-async def add_employee_to_structure(
-    employee: EmployeeStructureCreate, employee_service: EmployeeServiceDeps
+@router.post("/{department_id}/employees", dependencies=[OnlyAdminDeps], summary="Добавление сотрудника")
+async def add_employee_to_department(
+    department_id: int, employee: EmployeeStructureCreate, employee_service: EmployeeServiceDeps
 ) -> EmployeeResponse:
-    new_employee = await employee_service.add_employee(employee.model_dump(exclude_unset=True))
+    new_employee = await employee_service.add_employee(department_id, employee)
     return new_employee
 
 
-@router.get("/{employee_id}", summary="Получить работника")
-async def get_one(employee_id: int, employee_service: EmployeeServiceDeps) -> EmployeeResponse:
-    employee = await employee_service.get_employee(employee_id)
-    return employee
-
-
-@router.put("/{employee_id}", summary="Обновление данных сотрудника")
-async def update_employees(
-    employee_id: int, employee_data: EmployeeStructureUpdate, employee_service: EmployeeServiceDeps
+@router.get("/{department_id}/employees/{employee_id}", summary="Получить работника")
+async def get_department_employee(
+    department_id: int, employee_id: int, employee_service: EmployeeServiceDeps
 ) -> EmployeeResponse:
-    employee = await employee_service.update_employee(employee_id, employee_data.model_dump(exclude_unset=True))
+    employee = await employee_service.get_employee(department_id, employee_id)
     return employee
 
 
-@router.delete("/{employee_id}", summary="Удаление сотрудника")
-async def delete_employees(employee_id: int, employee_service: EmployeeServiceDeps) -> dict:
-    await employee_service.delete_employee(employee_id)
+@router.put("/{department_id}/employees/{employee_id}", summary="Обновление данных сотрудника")
+async def update_employee_position(
+    department_id: int, employee_id: int, employee_data: EmployeeStructureUpdate, employee_service: EmployeeServiceDeps
+) -> EmployeeResponse:
+    employee = await employee_service.update_position(department_id, employee_id, employee_data)
+    return employee
+
+
+@router.delete("/{department_id}/employees/{employee_id}", summary="Удаление сотрудника")
+async def delete_employees(department_id: int, employee_id: int, employee_service: EmployeeServiceDeps) -> dict:
+    await employee_service.delete_employee(department_id, employee_id)
     return {"message": "Работник удален"}
 
 
-@router.post("/add_managers/", summary="Назначение руководителя сотрудникам")
-async def add_manager(manager_data: AddManager, employee_service: EmployeeServiceDeps) -> dict:
-    await employee_service.add_manager(**manager_data.model_dump())
+@router.post("/{department_id}/add_manager/", summary="Назначение руководителя сотрудникам")
+async def add_manager(department_id: int, manager_data: AddManager, employee_service: EmployeeServiceDeps) -> dict:
+    await employee_service.add_manager(department_id, manager_data)
     return {"message": "Руководитель назначен"}
 
 
-@router.post("/add_extra_managers/", summary="Добавление дополнительного руководителя")
+@router.post(
+    "/{department_id}/employees/{employee_id}/add_extra_manager/", summary="Добавление дополнительного руководителя"
+)
 async def add_extra_manager(
-    manager: EmployeeManagerCreate, employee_manager_service: EmployeeManagerServiceDeps
+    department_id: int, employee_id: int, data: EmployeeManagerCreate, employee_manager_service: EmployeeServiceDeps
 ) -> EmployeeManagerResponse:
-    employee_manager = await employee_manager_service.add_manager(manager.model_dump())
+    employee_manager = await employee_manager_service.add_extra_manager(department_id, employee_id, data)
     return employee_manager
 
 
