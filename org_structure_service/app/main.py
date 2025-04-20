bdc2d5242117bc546d6b58f3ad9c32418b833e93
bissaliev <bissaliev21@gmail.com>
2025-04-20 10:52:57 +0400

    org_structure_service: Изменил префиксы в адресах маршрутов в главном модуле

--- org_structure_service/app/main.py
+++ org_structure_service/app/main.py
@@ -7,10 +7,10 @@ from app.routers.structures import router as structure_router
 
 app = FastAPI(root_path="/org")
 
-app.include_router(structure_router, prefix="/structures", tags=["structures"])
-app.include_router(division_router, prefix="/divisions", tags=["divisions"])
-app.include_router(department_router, prefix="/departments", tags=["departments"])
-app.include_router(employee_router, prefix="/employees", tags=["employees"])
+app.include_router(structure_router, prefix="/teams", tags=["teams"])
+app.include_router(division_router, prefix="/teams", tags=["divisions"])
+app.include_router(department_router, prefix="/teams", tags=["departments"])
+app.include_router(employee_router, prefix="/departments", tags=["employees"])
 
 
 if __name__ == "__main__":
