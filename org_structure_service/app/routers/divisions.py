c5cb240a88814d86584acab0d98daef9d96af23b
bissaliev <bissaliev21@gmail.com>
2025-04-20 11:28:50 +0400

    org_structure_service: Добавил в маршруты app/routers/divisions.py право доступа только с ролью админа в своей команде при записи

--- org_structure_service/app/routers/divisions.py
+++ org_structure_service/app/routers/divisions.py
@@ -4,7 +4,7 @@ from app.routers.dependencies import AdminAndAssigned, DivisionServiceDeps
 from app.schemas.divisions import DivisionCreate, DivisionResponse, DivisionUpdate
 from app.schemas.response import MessageDelete
 
-router = APIRouter(dependencies=[AdminAndAssigned])
+router = APIRouter()
 
 
 @router.get("/{team_id}/divisions", summary="Получение списка дивизий определенной команды")
@@ -12,7 +12,7 @@ async def get_divisions(team_id: int, division_service: DivisionServiceDeps) -> 
     return await division_service.get_divisions(team_id)
 
 
-@router.post("/{team_id}/divisions", summary="Создание дивизии")
+@router.post("/{team_id}/divisions", dependencies=[AdminAndAssigned], summary="Создание дивизии")
 async def create_division(
     team_id: int, division: DivisionCreate, division_service: DivisionServiceDeps
 ) -> DivisionResponse:
@@ -25,14 +25,14 @@ async def get_division(team_id: int, division_id: int, division_service: Divisio
     return await division_service.get_division(team_id, division_id)
 
 
-@router.patch("/{team_id}/divisions/{division_id}", summary="Обновление дивизии")
+@router.patch("/{team_id}/divisions/{division_id}", dependencies=[AdminAndAssigned], summary="Обновление дивизии")
 async def update_division(
     team_id: int, division_id: int, update_data: DivisionUpdate, division_service: DivisionServiceDeps
 ) -> DivisionResponse:
     return await division_service.update_division(team_id, division_id, update_data)
 
 
-@router.delete("/{team_id}/divisions/{division_id}", summary="Удаление дивизии")
+@router.delete("/{team_id}/divisions/{division_id}", dependencies=[AdminAndAssigned], summary="Удаление дивизии")
 async def delete_division(team_id: int, division_id: int, division_service: DivisionServiceDeps) -> MessageDelete:
     await division_service.delete_division(team_id, division_id)
     return MessageDelete()
