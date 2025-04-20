23a1d9fe8265b147f505273893ad5829cae037b4
bissaliev <bissaliev21@gmail.com>
2025-04-20 06:25:03 +0400

    org_structure_service: Добавил два метода в репозиторий DepartmentRepository (get_team_departments, get_team_department)

--- org_structure_service/app/repositories/department_repo.py
+++ org_structure_service/app/repositories/department_repo.py
@@ -1,6 +1,20 @@
+from sqlalchemy import select
+
 from app.models import Department
 from app.repositories.base_repository import BaseRepository
 
 
 class DepartmentRepository(BaseRepository):
+    """Репозиторий для модели Department"""
+
     model = Department
+
+    async def get_team_departments(self, team_id: int) -> list[Department]:
+        """Получение департаментов определенной команды"""
+        stmt = select(self.model).where(self.model.team_id == team_id)
+        return (await self.session.scalars(stmt)).all()
+
+    async def get_team_department(self, team_id: int, department_id: int) -> Department:
+        """Получение департаментов определенной команды"""
+        stmt = select(self.model).where(self.model.team_id == team_id, self.model.id == department_id)
+        return (await self.session.scalars(stmt)).first()
