5873dea13b8177a822510ca82b6697b12cb49515
bissaliev <bissaliev21@gmail.com>
2025-04-21 07:57:56 +0400

    teams_service: Добавил вебхук для регистрации работника по team_code

--- teams_service/app/main.py
+++ teams_service/app/main.py
@@ -7,6 +7,7 @@ from app.database import async_engine
 from app.routers.employees import router as employee_router
 from app.routers.team_news import router as team_news_router
 from app.routers.teams import router as team_router
+from app.routers.webhook import router as webhook
 
 app = FastAPI(root_path="/teams")
 admin = Admin(app, async_engine, authentication_backend=authentication_backend)
@@ -19,3 +20,4 @@ admin.add_view(TeamNewsAdmin)
 app.include_router(team_router, prefix="/teams", tags=["teams"])
 app.include_router(employee_router, prefix="/teams", tags=["employees"])
 app.include_router(team_news_router, prefix="/teams", tags=["team_news"])
+app.include_router(webhook, prefix="/webhook", tags=["webhook"])
