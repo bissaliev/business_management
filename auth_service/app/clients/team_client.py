c0ded2b84b5a1ed32efe00372254582b6ec366a9
bissaliev <bissaliev21@gmail.com>
2025-04-21 07:50:49 +0400

    auth_service: Рефакторинг в app/clients/team_client.py (Структурировал, добавил читаемые статус коды)

--- auth_service/app/clients/team_client.py
+++ auth_service/app/clients/team_client.py
@@ -1,5 +1,5 @@
 import httpx
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 
 from app.config import settings
 from app.schemas.users import TeamRoleResponse
@@ -8,22 +8,43 @@ BASE_TEAM_URL = settings.get_team_url()
 
 
 class TeamServiceClient:
+    """Клиент для взаимодействия с Team Service"""
+
     def __init__(self, base_url: str = BASE_TEAM_URL):
         self.base_url = base_url
+        self.headers = {"X-API-KEY": settings.TEAM_API_KEY}
+        self.timeout = httpx.Timeout(10.0, connect=5.0)
 
-    async def get_employee_role(self, user_id: int, team_id: int):
-        """Получение работника из Team Service"""
+    async def _request(self, method: str, url: str, **kwargs):
         try:
-            async with httpx.AsyncClient(timeout=httpx.Timeout(10.0, connect=5.0)) as client:
-                response = await client.get(f"{self.base_url}/teams/{team_id}/employees/{user_id}")
+            async with httpx.AsyncClient(timeout=self.timeout) as client:
+                response = await client.request(method, url, headers=self.headers, **kwargs)
                 response.raise_for_status()
-                return TeamRoleResponse.model_validate(response.json())
+                return response
         except httpx.HTTPStatusError as e:
-            if e.response.status_code == 404:
-                raise HTTPException(status_code=404, detail=f"Работник с {user_id} не существует") from e
+            status_code = e.response.status_code
+            if status_code == status.HTTP_404_NOT_FOUND:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Ресурс не найден") from e
+            if status_code == status.HTTP_400_BAD_REQUEST:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Неверные входные данные") from e
             raise HTTPException(
-                status_code=500,
-                detail=f"Ошибка при получении работника: {e.response.status_code}, {e.response.text}",
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Ошибка при обращении к Team Service: {status_code}, {e.response.text}",
             ) from e
         except httpx.ConnectError as e:
-            raise HTTPException(status_code=503, detail="Сервис команд не доступен") from e
+            raise HTTPException(
+                status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Сервис команд не доступен"
+            ) from e
+
+    async def get_employee_role(self, user_id: int, team_id: int):
+        """Получение роли работника из Team Service"""
+        url = f"{self.base_url}/teams/{team_id}/employees/{user_id}"
+        response = await self._request("GET", url)
+        return TeamRoleResponse.model_validate(response.json())
+
+    async def add_employee_to_team(self, employee_id: int, team_code: str):
+        """Добавление работника в команду через Team Service"""
+        url = f"{self.base_url}/webhook/add_employee/"
+        payload = {"team_code": team_code, "employee_id": employee_id}
+        response = await self._request("POST", url, json=payload)
+        return response.json()
