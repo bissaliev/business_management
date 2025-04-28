069bf8153aeccc67bf089996b6f5c3c3fe05de63
bissaliev <bissaliev21@gmail.com>
2025-04-28 20:45:34 +0400

    auth_service: Fix: Присвоил тип user: type[User]; Добавил utc в now; Добавил session.commit в методы soft_delete, restore

--- auth_service/app/repositories/user_repo.py
+++ auth_service/app/repositories/user_repo.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from sqlalchemy import select, update
 
@@ -9,7 +9,7 @@ from app.repositories.base_repository import BaseRepository
 class UserRepository(BaseRepository):
     """Репозиторий для пользователей"""
 
-    model = User
+    model: type[User] = User
 
     async def get_user_by_email(self, email: str) -> User | None:
         """Получение пользователя по полю email"""
@@ -18,10 +18,16 @@ class UserRepository(BaseRepository):
 
     async def soft_delete(self, id: int) -> None:
         """Удаление пользователя с возможностью восстановления"""
-        stmt = update(self.model).where(self.model.id == id).values(is_active=False, deleted_at=datetime.now())
+        stmt = (
+            update(self.model)
+            .where(self.model.id == id)
+            .values(is_active=False, deleted_at=datetime.now(timezone.utc))
+        )
         await self.session.execute(stmt)
+        self.session.commit()
 
     async def restore(self, id: int) -> None:
         """Восстановление пользователя"""
         stmt = update(self.model).where(self.model.id == id).values(is_active=True, deleted_at=None)
         await self.session.execute(stmt)
+        self.session.commit()
