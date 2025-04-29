097b23096c69583f54151d7d04d09781075ee09d
bissaliev <bissaliev21@gmail.com>
2025-04-29 15:58:20 +0400

    auth_service: Fix: Добавил в маршруты app/routers/users.py response_model; Добавил параметр {id} в update_user, soft_delete_user; Добавил статусы

--- auth_service/app/routers/users.py
+++ auth_service/app/routers/users.py
@@ -1,33 +1,60 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-from app.routers.dependencies import CurrentUserDeps, UserServiceDeps
-from app.schemas.users import Message, UserResponse, UserRestore, UserUpdate
+from app.routers.dependencies import CurrentUserDeps, RequiredAdminDeps, UserServiceDeps
+from app.schemas.users import UserCreate, UserResponse, UserUpdate
 
 router = APIRouter()
 
 
-@router.get("/", summary="Получение пользователей")
+@router.post("/register", response_model=UserResponse, summary="Регистрация пользователя")
+async def register_user(user_data: UserCreate, user_service: UserServiceDeps) -> UserResponse:
+    return await user_service.create(user_data.model_dump())
+
+
+@router.get("/", response_model=list[UserResponse], summary="Получение пользователей")
 async def get_users(user_service: UserServiceDeps) -> list[UserResponse]:
     return await user_service.get_users()
 
 
-@router.get("/{id}", summary="Получение пользователя")
+@router.get("/{id}", response_model=UserResponse, dependencies=[RequiredAdminDeps], summary="Получение пользователя")
 async def get_user(id: int, user_service: UserServiceDeps) -> UserResponse:
     return await user_service.get_user(id)
 
 
-@router.put("/update", summary="Обновление данных пользователя")
-async def update_user(user: CurrentUserDeps, user_data: UserUpdate, user_service: UserServiceDeps) -> UserResponse:
-    return await user_service.update(user.id, user_data.model_dump(exclude_unset=True))
+@router.put(
+    "/{id}", response_model=UserResponse, dependencies=[RequiredAdminDeps], summary="Обновление данных пользователя"
+)
+async def update_user(id: int, user_data: UserUpdate, user_service: UserServiceDeps) -> UserResponse:
+    return await user_service.update(id, user_data.model_dump(exclude_unset=True))
 
 
-@router.delete("/delete", summary="Удаление пользователя с возможность восстановления")
-async def soft_delete_user(user: CurrentUserDeps, user_service: UserServiceDeps) -> Message:
-    await user_service.soft_delete_user(user.id)
-    return Message(message="Пользователь временно удален")
+@router.delete(
+    "/{id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[RequiredAdminDeps],
+    summary="Удаление пользователя с возможность восстановления",
+)
+async def soft_delete_user(id: int, user_service: UserServiceDeps) -> None:
+    await user_service.soft_delete_user(id)
 
 
-@router.post("/restore", summary="Восстановление пользователя")
-async def restore_user(data: UserRestore, user_service: UserServiceDeps) -> Message:
-    await user_service.restore_user(data.model_dump())
-    return Message(message="Пользователь восстановлен")
+@router.patch(
+    "/{id}/restore",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[RequiredAdminDeps],
+    summary="Восстановление пользователя",
+)
+async def restore_user(id: int, user_service: UserServiceDeps) -> None:
+    await user_service.restore_user(id)
+
+
+@router.get("/me/", response_model=UserResponse, summary="Получение профиля пользователя")
+async def read_users_me(current_user: CurrentUserDeps, user_service: UserServiceDeps) -> UserResponse:
+    return await user_service.get_user(current_user.id)
+
+
+@router.put("/me/", response_model=UserResponse, summary="Обновление профиля пользователя")
+async def update_users_me(
+    current_user: CurrentUserDeps, user_data: UserUpdate, user_service: UserServiceDeps
+) -> UserResponse:
+    return await user_service.update(current_user.id, user_data.model_dump(exclude_unset=True))
