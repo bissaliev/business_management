2d61f47974a1cdb7164f29e248f48c24ddca4dd9
bissaliev <bissaliev21@gmail.com>
2025-04-30 17:18:46 +0400

    org_structure_service: Fix: Добавил response_model и статусы в маршруты app/routers/employees.py

--- org_structure_service/app/routers/employees.py
+++ org_structure_service/app/routers/employees.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.routers.dependencies import AdminAndAssignedDepartmentDeps, EmployeeServiceDeps
 from app.schemas.employees import (
@@ -13,14 +13,19 @@ from app.schemas.employees import (
 router = APIRouter()
 
 
-@router.get("/{department_id}/employees", summary="Список всех сотрудников департамента")
+@router.get(
+    "/{department_id}/employees", response_model=list[EmployeeResponse], summary="Список всех сотрудников департамента"
+)
 async def get_employees(department_id: int, employee_service: EmployeeServiceDeps) -> list[EmployeeResponse]:
     employees = await employee_service.get_employees(department_id)
     return employees
 
 
 @router.post(
-    "/{department_id}/employees", dependencies=[AdminAndAssignedDepartmentDeps], summary="Добавление сотрудника"
+    "/{department_id}/employees",
+    response_model=EmployeeResponse,
+    dependencies=[AdminAndAssignedDepartmentDeps],
+    summary="Добавление сотрудника",
 )
 async def add_employee_to_department(
     department_id: int, employee: EmployeeStructureCreate, employee_service: EmployeeServiceDeps
@@ -29,7 +34,7 @@ async def add_employee_to_department(
     return new_employee
 
 
-@router.get("/{department_id}/employees/{employee_id}", summary="Получить работника")
+@router.get("/{department_id}/employees/{employee_id}", response_model=EmployeeResponse, summary="Получить работника")
 async def get_department_employee(
     department_id: int, employee_id: int, employee_service: EmployeeServiceDeps
 ) -> EmployeeResponse:
@@ -40,6 +45,7 @@ async def get_department_employee(
 @router.put(
     "/{department_id}/employees/{employee_id}",
     dependencies=[AdminAndAssignedDepartmentDeps],
+    response_model=EmployeeResponse,
     summary="Обновление данных сотрудника",
 )
 async def update_employee_position(
@@ -52,26 +58,27 @@ async def update_employee_position(
 @router.delete(
     "/{department_id}/employees/{employee_id}",
     dependencies=[AdminAndAssignedDepartmentDeps],
+    status_code=status.HTTP_204_NO_CONTENT,
     summary="Удаление сотрудника",
 )
-async def delete_employees(department_id: int, employee_id: int, employee_service: EmployeeServiceDeps) -> dict:
+async def delete_employees(department_id: int, employee_id: int, employee_service: EmployeeServiceDeps) -> None:
     await employee_service.delete_employee(department_id, employee_id)
-    return {"message": "Работник удален"}
 
 
 @router.post(
     "/{department_id}/add_manager/",
     dependencies=[AdminAndAssignedDepartmentDeps],
+    status_code=status.HTTP_204_NO_CONTENT,
     summary="Назначение руководителя сотрудникам",
 )
-async def add_manager(department_id: int, manager_data: AddManager, employee_service: EmployeeServiceDeps) -> dict:
+async def add_manager(department_id: int, manager_data: AddManager, employee_service: EmployeeServiceDeps) -> None:
     await employee_service.add_manager(department_id, manager_data)
-    return {"message": "Руководитель назначен"}
 
 
 @router.post(
     "/{department_id}/employees/{employee_id}/add_extra_manager/",
     dependencies=[AdminAndAssignedDepartmentDeps],
+    response_model=EmployeeManagerResponse,
     summary="Добавление дополнительного руководителя",
 )
 async def add_extra_manager(
@@ -81,7 +88,11 @@ async def add_extra_manager(
     return employee_manager
 
 
-@router.get("/{employee_id}/department_members", summary="Список коллег департамента сотрудника")
+@router.get(
+    "/{employee_id}/department_members",
+    response_model=list[EmployeeResponse],
+    summary="Список коллег департамента сотрудника",
+)
 async def get_department_members(employee_id: int, employee_service: EmployeeServiceDeps) -> list[EmployeeResponse]:
     employees = await employee_service.get_department_members(employee_id)
     return employees
