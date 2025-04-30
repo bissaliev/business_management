9031a2589ba4ed4b0815b6f8092d709c3be81bd0
bissaliev <bissaliev21@gmail.com>
2025-04-30 10:02:16 +0400

    calendar_service: Fix: Добавил логирование в app/clients/user_client.py

--- calendar_service/app/clients/user_client.py
+++ calendar_service/app/clients/user_client.py
@@ -2,11 +2,14 @@ import httpx
 from fastapi import HTTPException
 
 from app.config import settings
+from app.logging_config import logger
 
 BASE_URL = settings.get_user_url()
 
 
 class UserServiceClient:
+    """Клиент для взаимодействия с сервисом User Service"""
+
     def __init__(self, base_url: str = BASE_URL):
         self.base_url = base_url
 
@@ -20,6 +23,8 @@ class UserServiceClient:
                 response.raise_for_status()
                 return response.json()
         except httpx.HTTPStatusError as e:
+            logger.error(f"Ошибка при верификации токена: {e}")
             raise HTTPException(status_code=e.response.status_code, detail="Неверный токен или пользователь") from e
         except httpx.ConnectError as e:
+            logger.error(f"Сервис User service не доступен: {e}")
             raise HTTPException(status_code=503, detail="Сервис User service не доступен") from e
