ff84b648844532861ee0d84f2466ebd5dbbc781c
bissaliev <bissaliev21@gmail.com>
2025-04-30 09:38:32 +0400

    calendar_service: Fix: Добавил аннотации в маршруты app/routers/calendar.py

--- calendar_service/app/routers/calendar.py
+++ calendar_service/app/routers/calendar.py
@@ -12,11 +12,13 @@ router = APIRouter()
 @router.get("/month", response_model=list[CalendarMonthResponse])
 async def get_monthly_calendar(
     params: Annotated[YearMonthParams, Depends()], user: CurrentUser, calendar_service: CalendarServiceDeps
-):
+) -> list[CalendarMonthResponse]:
     calendar = await calendar_service.get_monthly_events(params, user)
     return calendar
 
 
 @router.get("/day", response_model=CalendarDayResponse)
-async def get_daily_calendar(date: date, user: CurrentUser, calendar_service: CalendarServiceDeps):
+async def get_daily_calendar(
+    date: date, user: CurrentUser, calendar_service: CalendarServiceDeps
+) -> CalendarDayResponse:
     return await calendar_service.get_daily_events(date, user)
