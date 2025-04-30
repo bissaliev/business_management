5ade0676dc2e197ca4354fcd96e0833c1b117e60
bissaliev <bissaliev21@gmail.com>
2025-04-30 16:34:37 +0400

    meeting_service: Fix: Добавил response_model и статусы в маршруты app/routers/meetings.py

--- meetings_service/app/routers/meetings.py
+++ meetings_service/app/routers/meetings.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.routers.dependencies import CurrentUser, MeetingServiceDeps
 from app.schemas.meeting import MeetingCreate, MeetingOut, MeetingParticipantOut, MeetingUpdate, Participant
-from app.schemas.responses import MessageMeetingDelete, MessageParticipantAdd, MessageParticipantDelete
 
 router = APIRouter()
 
 
-@router.post("/", summary="Создание встречи")
+@router.post("/", response_model=MeetingOut, summary="Создание встречи")
 async def create_meeting(
     meeting_data: MeetingCreate, user: CurrentUser, meeting_service: MeetingServiceDeps
 ) -> MeetingOut:
@@ -16,7 +15,7 @@ async def create_meeting(
     return meeting
 
 
-@router.get("/", summary="Список встреч текущего пользователя")
+@router.get("/", response_model=list[MeetingParticipantOut], summary="Список встреч текущего пользователя")
 async def list_meetings(
     user: CurrentUser, meeting_service: MeetingServiceDeps, team_id: int | None = None
 ) -> list[MeetingParticipantOut]:
@@ -24,7 +23,7 @@ async def list_meetings(
     return meetings
 
 
-@router.get("/{meeting_id}", summary="Поучение встречи")
+@router.get("/{meeting_id}", response_model=MeetingParticipantOut, summary="Поучение встречи")
 async def get_meeting(
     meeting_id: int, user: CurrentUser, meeting_service: MeetingServiceDeps
 ) -> MeetingParticipantOut:
@@ -32,7 +31,7 @@ async def get_meeting(
     return meeting
 
 
-@router.put("/{meeting_id}", summary="Обновление данных встречи")
+@router.put("/{meeting_id}", response_model=MeetingOut, summary="Обновление данных встречи")
 async def update_meeting(
     meeting_id: int, user: CurrentUser, update_data: MeetingUpdate, meeting_service: MeetingServiceDeps
 ) -> MeetingOut:
@@ -40,25 +39,26 @@ async def update_meeting(
     return meeting
 
 
-@router.delete("/{meeting_id}", summary="Отмена встречи")
-async def delete_meeting(
-    meeting_id: int, user: CurrentUser, meeting_service: MeetingServiceDeps
-) -> MessageMeetingDelete:
+@router.delete("/{meeting_id}", status_code=status.HTTP_204_NO_CONTENT, summary="Отмена встречи")
+async def delete_meeting(meeting_id: int, user: CurrentUser, meeting_service: MeetingServiceDeps):
     await meeting_service.delete_meeting(user, meeting_id)
-    return MessageMeetingDelete()
 
 
-@router.post("/{meeting_id}/participants/", summary="Добавление участника встречи")
+@router.post(
+    "/{meeting_id}/participants/", status_code=status.HTTP_204_NO_CONTENT, summary="Добавление участника встречи"
+)
 async def add_participant(
     meeting_id: int, user: CurrentUser, participant: Participant, meeting_service: MeetingServiceDeps
-) -> MessageParticipantAdd:
+) -> None:
     await meeting_service.add_participant(meeting_id, participant, user)
-    return MessageParticipantAdd()
 
 
-@router.delete("/{meeting_id}/participants/{participant_id}", summary="Удаление участника встречи")
+@router.delete(
+    "/{meeting_id}/participants/{participant_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Удаление участника встречи",
+)
 async def delete_participant(
     meeting_id: int, participant_id: int, user: CurrentUser, meeting_service: MeetingServiceDeps
-) -> MessageParticipantDelete:
+) -> None:
     await meeting_service.delete_participant(meeting_id, participant_id, user)
-    return MessageParticipantDelete()
