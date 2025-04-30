45f7879e038e823198935638660625449ee3a59a
bissaliev <bissaliev21@gmail.com>
2025-04-30 09:36:34 +0400

    calendar_service: Fix: Добавил логирование в репозиторий EventRepository при удалении объекта; поправил аннотации; добавил название метода отражающего суть операции;

--- calendar_service/app/repositories/event_repo.py
+++ calendar_service/app/repositories/event_repo.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from sqlalchemy import delete, exists, select
 
+from app.logging_config import logger
 from app.models.events import Event
 from app.repositories.base_repository import BaseRepository
 
@@ -9,34 +10,39 @@ from app.repositories.base_repository import BaseRepository
 class EventRepository(BaseRepository):
     """Репозиторий управления событиями"""
 
-    model = Event
+    model: type[Event] = Event
 
     async def get_by_period(self, employee_id: int, start_time: datetime, end_time: datetime) -> list[Event]:
         """Получение событий за определенный период"""
         stmt = select(self.model).where(
             self.model.employee_id == employee_id,
-            self.model.start_time >= start_time,
             self.model.start_time < end_time,
+            self.model.end_time > start_time,
         )
         return (await self.session.scalars(stmt)).all()
 
     async def delete_by_source(self, source_id: int, event_type: str, employee_id: int) -> None:
         """Удаление события по source_id, event_type, employee_id"""
+        logger.info(
+            f"Удаление события с идентификатором источника={source_id}, "
+            f"типом события={event_type}, идентификатором сотрудника={employee_id}"
+        )
         stmt = delete(self.model).where(
             self.model.source_id == source_id,
             self.model.event_type == event_type,
             self.model.employee_id == employee_id,
         )
-        await self.session.execute(stmt)
+        result = await self.session.execute(stmt)
         await self.session.commit()
+        logger.info(f"Успешно удаленно {result.rowcount} записи из {self.model.__name__}")
 
-    async def verify_event(self, employee_id: int, start_time: datetime, end_time: datetime) -> list[Event]:
+    async def has_events_in_period(self, employee_id: int, start_time: datetime, end_time: datetime) -> bool:
         """Запрос выполняет проверку на существования события за определенный период"""
         stmt = select(
-            exists(self.model).where(
+            exists().where(
                 self.model.employee_id == employee_id,
-                self.model.start_time >= start_time,
                 self.model.start_time < end_time,
+                self.model.end_time > start_time,
             )
         )
-        return await self.session.scalar(stmt)
+        return bool(await self.session.scalar(stmt))
