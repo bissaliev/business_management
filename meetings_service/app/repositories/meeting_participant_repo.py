fa213e0e9d71d17da22766bef047c0e8afd21cc9
bissaliev <bissaliev21@gmail.com>
2025-05-01 08:40:01 +0400

    meeting_service: Fix: Добавил аннотации в app/repositories/meeting_participant_repo.py

--- meetings_service/app/repositories/meeting_participant_repo.py
+++ meetings_service/app/repositories/meeting_participant_repo.py
@@ -7,12 +7,12 @@ from app.repositories.base_repository import BaseRepository
 class MeetingParticipantRepository(BaseRepository):
     """Репозиторий для управления участниками встреч"""
 
-    model = MeetingParticipant
+    model: type[MeetingParticipant] = MeetingParticipant
 
     async def exists_participant(self, meeting_id: int, participant_id: int) -> bool:
         """Проверка на существование участника во встрече"""
         stmt = select(exists().where(self.model.meeting_id == meeting_id, self.model.participant_id == participant_id))
-        return await self.session.scalar(stmt)
+        return bool(await self.session.scalar(stmt))
 
     async def delete_participant(self, meeting_id: int, participant_id: int) -> bool:
         """Удаление участника из встречи"""
