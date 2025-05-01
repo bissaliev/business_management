b3667c5f1d0f86fe24b7bf0169e2f91c7e40bbef
bissaliev <bissaliev21@gmail.com>
2025-05-01 06:09:28 +0400

    tasks_service: Fix: Добавил аннотации, логирование в app/services/comment_service.py

--- tasks_service/app/services/comment_service.py
+++ tasks_service/app/services/comment_service.py
@@ -1,7 +1,9 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.logging_config import logger
 from app.models.comments import Comment
+from app.models.tasks import Task
 from app.repositories.comment_repo import CommentRepository
 from app.repositories.task_repo import TaskRepository
 from app.schemas.comments import CommentCreate
@@ -20,13 +22,18 @@ class CommentService:
         """Создание комментариев"""
         task = await self._get_task_or_404(task_id)
         comment = await self.repo.create(task_id=task.id, user_id=user.id, **comment_data.model_dump())
+        await self.session.commit()
+        logger.info(f"Создан комментарий {comment.id} к задаче {task_id}")
         return comment
 
     async def update_comment(self, task_id: int, comment_id: int, user: User, comment_data: CommentCreate) -> Comment:
         """Обновление комментариев"""
         await self._get_task_or_404(task_id)
         await self._require_author(comment_id, user.id)
-        return await self.repo.update(comment_id, **comment_data.model_dump(exclude_unset=True))
+        comment = await self.repo.update(comment_id, **comment_data.model_dump(exclude_unset=True))
+        await self.session.commit()
+        logger.info(f"Обновлен комментарий {comment_id} к задаче {task_id}")
+        return comment
 
     async def get_comments_by_task_id(self, task_id: int) -> list[Comment]:
         """Получение всех комментариев задачи"""
@@ -38,22 +45,24 @@ class CommentService:
         await self._get_task_or_404(task_id)
         await self._require_author(comment_id, user.id)
         await self.repo.delete(comment_id)
+        await self.session.commit()
+        logger.info(f"Удален комментарий {comment_id} к задаче {task_id}")
 
-    async def _get_task_or_404(self, task_id):
+    async def _get_task_or_404(self, task_id) -> Task:
         """Получить объект Task или вызвать исключение HTTP_404_NOT_FOUND"""
         task = await self.task_repo.get(task_id)
         if not task:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
         return task
 
-    async def _get_comment_or_404(self, comment_id):
+    async def _get_comment_or_404(self, comment_id) -> Comment:
         """Получить объект Comment или вызвать исключение HTTP_404_NOT_FOUND"""
         comment = await self.repo.get(comment_id)
         if not comment:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Комментарий не найден")
         return comment
 
-    async def _require_author(self, comment_id: int, user_id: int):
+    async def _require_author(self, comment_id: int, user_id: int) -> Comment:
         """Получить объект Comment если пользователь автор комментария"""
         comment = await self._get_comment_or_404(comment_id)
         if comment.user_id != user_id:
