7b0b61c26c1f9b2d1cf4e629ed3c9b0bc82d2d28
bissaliev <bissaliev21@gmail.com>
2025-05-01 08:21:06 +0400

    org_structure_service: Fix: Добавил аннотации в репозиторий app/repositories/employee_repo.py

--- org_structure_service/app/repositories/employee_repo.py
+++ org_structure_service/app/repositories/employee_repo.py
@@ -5,15 +5,15 @@ from app.repositories.base_repository import BaseRepository
 
 
 class EmployeeRepository(BaseRepository):
-    model = EmployeeStructure
+    """Репозиторий для управления работниками"""
 
-    async def exists_in_department(self, employee_id: int, department_id: int):
+    model: type[EmployeeStructure] = EmployeeStructure
+
+    async def exists_in_department(self, employee_id: int, department_id: int) -> bool:
         """Проверка на существование связи работник-департамент"""
-        stmt = select(
-            exists(self.model).where(self.model.employee_id == employee_id, self.model.department_id == department_id)
-        )
+        stmt = select(exists().where(self.model.employee_id == employee_id, self.model.department_id == department_id))
         result = await self.session.execute(stmt)
-        return result.scalar()
+        return bool(result.scalar())
 
     async def get_employees_by_department_id(self, department_id: int) -> list[EmployeeStructure]:
         """Получить список сотрудников по идентификатору департамента"""
@@ -21,35 +21,38 @@ class EmployeeRepository(BaseRepository):
         result = await self.session.scalars(stmt)
         return result.all()
 
-    async def get_employee(self, department_id: int, employee_id: int):
+    async def get_employee(self, department_id: int, employee_id: int) -> EmployeeStructure:
         """Получить сотрудника"""
         stmt = select(self.model).where(
             self.model.department_id == department_id, self.model.employee_id == employee_id
         )
         return (await self.session.scalars(stmt)).first()
 
-    async def get_employee_by_employee_id(self, employee_id: int):
+    async def get_employee_by_employee_id(self, employee_id: int) -> EmployeeStructure:
+        """Получение работника по employee_id"""
         stmt = select(self.model).where(self.model.employee_id == employee_id)
         return (await self.session.scalars(stmt)).first()
 
     async def update_employee(self, employee_id: int, **data: dict) -> EmployeeStructure:
+        """Обновление работника по employee_id"""
         stmt = update(self.model).where(self.model.employee_id == employee_id).values(**data).returning(self.model)
         result = await self.session.scalars(stmt)
         return result.first()
 
     async def delete_employee(self, employee_id: int, department_id: int) -> bool:
+        """Удаление работника по employee_id"""
         stmt = delete(self.model).where(
             self.model.employee_id == employee_id, self.model.department_id == department_id
         )
         result = await self.session.execute(stmt)
         return result.rowcount > 0
 
-    async def add_manager(self, department_id: int, manager_id):
-        """Получить сотрудника"""
+    async def add_manager(self, department_id: int, manager_id) -> None:
+        """Назначить менеджера работникам из одного департамента"""
         stmt = (
             update(self.model)
             .where(self.model.department_id == department_id)
             .values(manager_id=manager_id)
             .returning(self.model)
         )
-        return (await self.session.scalars(stmt)).all()
+        await self.session.scalars(stmt)
