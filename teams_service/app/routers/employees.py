9c03bf27c8d12e5fedb4ecab7555619ce62fbd1b
bissaliev <bissaliev21@gmail.com>
2025-05-01 07:25:58 +0400

    teams_service: Fix: Добавил response_model и статусы в маршруты app/routers/employees.py

--- teams_service/app/routers/employees.py
+++ teams_service/app/routers/employees.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.routers.dependencies import AdminDeps, TeamEmployeeServiceDeps
 from app.schemas.employees import EmployeeCreate, EmployeeUpdateRole, TeamEmployeeResponse
@@ -6,7 +6,12 @@ from app.schemas.employees import EmployeeCreate, EmployeeUpdateRole, TeamEmploy
 router = APIRouter()
 
 
-@router.post("/{team_id}/employees/", dependencies=[AdminDeps], summary="Регистрация сотрудника")
+@router.post(
+    "/{team_id}/employees/",
+    response_model=TeamEmployeeResponse,
+    dependencies=[AdminDeps],
+    summary="Регистрация сотрудника",
+)
 async def create_employee(
     team_id: int, team_emp_service: TeamEmployeeServiceDeps, employee_data: EmployeeCreate
 ) -> TeamEmployeeResponse:
@@ -14,27 +19,43 @@ async def create_employee(
 
 
 @router.get(
-    "/{team_id}/employees/", dependencies=[AdminDeps], summary="Получение всех сотрудников определенной команды"
+    "/{team_id}/employees/",
+    response_model=list[TeamEmployeeResponse],
+    dependencies=[AdminDeps],
+    summary="Получение всех сотрудников определенной команды",
 )
 async def get_team_employees(team_emp_service: TeamEmployeeServiceDeps, team_id: int) -> list[TeamEmployeeResponse]:
     return await team_emp_service.get_team_employees(team_id)
 
 
-@router.get("/{team_id}/employees/{employee_id}", summary="Получение сотрудника определенной команды")
+@router.get(
+    "/{team_id}/employees/{employee_id}",
+    response_model=TeamEmployeeResponse,
+    summary="Получение сотрудника определенной команды",
+)
 async def get_specific_team_employee(
     team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int
 ) -> TeamEmployeeResponse:
     return await team_emp_service.get_specific_team_employee(team_id, employee_id)
 
 
-@router.patch("/{team_id}/employees/{employee_id}", dependencies=[AdminDeps], summary="Обновление роли работника")
+@router.patch(
+    "/{team_id}/employees/{employee_id}",
+    response_model=TeamEmployeeResponse,
+    dependencies=[AdminDeps],
+    summary="Обновление роли работника",
+)
 async def update_status(
     team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int, role: EmployeeUpdateRole
 ) -> TeamEmployeeResponse:
     return await team_emp_service.update_role(team_id, employee_id, role)
 
 
-@router.delete("/{team_id}/employees/{employee_id}", dependencies=[AdminDeps], summary="Удаление сотрудника")
-async def delete_employee(team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int) -> dict[str, str]:
+@router.delete(
+    "/{team_id}/employees/{employee_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[AdminDeps],
+    summary="Удаление сотрудника",
+)
+async def delete_employee(team_emp_service: TeamEmployeeServiceDeps, team_id: int, employee_id: int) -> None:
     await team_emp_service.delete_employee(team_id, employee_id)
-    return {"message": "Работник удален"}
