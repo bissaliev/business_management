311f65168e064e59b627525c0f684c707da4396f
bissaliev <bissaliev21@gmail.com>
2025-05-01 07:21:17 +0400

    teams_service: Fix: Добавил аннотации в репозиторий app/repositories/team_repo.py

--- teams_service/app/repositories/team_repo.py
+++ teams_service/app/repositories/team_repo.py
@@ -7,7 +7,7 @@ from app.repositories.base_repository import BaseRepository
 class TeamRepository(BaseRepository):
     """Репозиторий команд"""
 
-    model = Team
+    model: type[Team] = Team
 
     async def get_team_by_team_code(self, team_code: str) -> Team:
         """Получение команды по team_code"""
