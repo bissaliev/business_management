b8817772ae5df6fa2bee938e2a266849cae0912e
bissaliev <bissaliev21@gmail.com>
2025-05-01 06:30:49 +0400

    tasks_service: Fix: Добавил utc в поля модели Comment связанные сос временем

--- tasks_service/app/models/comments.py
+++ tasks_service/app/models/comments.py
@@ -1,7 +1,6 @@
-# task_service/models/comment.py
-from datetime import datetime
+from datetime import datetime, timezone
 
-from sqlalchemy import ForeignKey, Text
+from sqlalchemy import DateTime, ForeignKey, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -14,4 +13,4 @@ class Comment(Base):
     task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id", ondelete="CASCADE"), nullable=False)
     user_id: Mapped[int]  # Автор комментария
     content: Mapped[str] = mapped_column(Text, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
