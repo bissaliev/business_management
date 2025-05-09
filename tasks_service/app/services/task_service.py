62606e0829118155727e1cd0aca5eb877e45d40a
bissaliev <bissaliev21@gmail.com>
2025-05-09 08:51:51 +0400

    tasks_service: Fix: Добавил метод __init__, update_task  в класс TaskService для динамической передачи данных и обновления событий

--- tasks_service/app/services/task_service.py
+++ tasks_service/app/services/task_service.py
@@ -6,13 +6,22 @@ from app.services.event_mixin import EventMixin
 class TaskService(BaseTaskService, EventMixin):
     """Сервис для управления задачами с отправкой событий при добавлении и удалении задач в Calendar Service"""
 
+    def __init__(self, /, session, rmq_producer):
+        super().__init__(session=session, rmq_producer=rmq_producer)
+
     async def create_task(self, user, task_data) -> Task:
         """Создание задачи с добавление события в Calendar Service"""
         task = await super().create_task(user, task_data)
-        await self.create_event(task, user.id)
+        await self.create_event(task)
         return task
 
     async def delete_task(self, user, task_id) -> None:
         """Удаление задачи с удалением события из Calendar Service"""
         deleted_task = await super().delete_task(user, task_id)
-        await self.delete_event(deleted_task, user.id)
+        await self.delete_event(deleted_task)
+
+    async def update_task(self, task_id, update_data) -> Task:
+        """Обновление задачи с обновлением события в Calendar Service"""
+        updated_task = await super().update_task(task_id, update_data)
+        await self.update_event(updated_task)
+        return updated_task
