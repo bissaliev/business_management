be4931c3c9288578869b2fe5ba1e259c5d2e757a
bissaliev <bissaliev21@gmail.com>
2025-05-13 09:25:02 +0400

    calendar_service: Подключил EventConsumer в главный модуль через lifespan

--- calendar_service/app/main.py
+++ calendar_service/app/main.py
@@ -1,10 +1,29 @@
+import asyncio
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.events.event_consumer import EventConsumer
+from app.logging_config import logger
 from app.routers.calendar import router as calendar_router
 from app.routers.events import router as event_router
 from app.routers.webhooks import router as webhook_router
 
-app = FastAPI(root_path="/calendar-service")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with EventConsumer("calendar_events") as consumer:
+        task = asyncio.create_task(consumer.start_consumer())
+        await asyncio.sleep(0)
+        yield
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            logger.info("Фоновая задача RabbitMQ остановлена")
+
+
+app = FastAPI(root_path="/calendar-service", lifespan=lifespan)
 
 
 app.include_router(event_router, prefix="/events", tags=["events"])
