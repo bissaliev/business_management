bc73e33a65c6742eed1025d395082d4f06255ce7
bissaliev <bissaliev21@gmail.com>
2025-05-14 13:57:38 +0400

    meeting_service: Изменил app/services/event_mixin.EventMixin для публикации событий в RabbitMQ

--- meetings_service/app/services/event_mixin.py
+++ meetings_service/app/services/event_mixin.py
@@ -1,23 +1,42 @@
 from app.clients.calendar_service_client import CalendarServiceClient
+from app.clients.rabbitmq.event_publisher import EventPublisher
 from app.logging_config import logger
 from app.models.meeting import Meeting
 from app.schemas.event_webhooks import NewEventHook
+from app.schemas.meeting_events import EventType, MeetingCreatedEvent, MeetingDeletedEvent
 
 
 class EventMixin:
     """Класс миксин добавляет методы для управления событиями в Calendar Service"""
 
+    rmq_producer: EventPublisher | None = None
     calendar_client = CalendarServiceClient()
 
-    async def create_event(self, meeting: Meeting, user_id: int):
-        """Создание события в Calendar Service"""
-        event_data = NewEventHook.model_validate(meeting, by_alias=True).model_dump(
-            mode="json", exclude=["employee_id"]
-        )
-        event_data["employee_id"] = user_id
-        await self.calendar_client.send_meeting_webhook(event_data)
-        logger.info(f"Создание события для встречи {meeting.id=} в Calendar Service")
+    def add_producer(self, rmq_producer: EventPublisher) -> None:
+        self.rmq_producer = rmq_producer
 
+    async def create_event(self, meeting: Meeting) -> None:
+        """Создание события в Calendar Service"""
+        payload = MeetingCreatedEvent.model_validate(meeting, by_alias=True).model_dump(mode="json")
+        event = {"type": EventType.CREATED.value, "payload": payload}
+        await self.rmq_producer.publish(event)
+        logger.info(f"Событие о создании встречи {meeting.id} отправлено в сервисе Calendar Service")
+
+    async def delete_event(self, meeting: Meeting) -> None:
+        """Удаление события из Calendar Service"""
+        payload = MeetingDeletedEvent.model_validate(meeting, by_alias=True).model_dump(mode="json")
+        event = {"type": EventType.DELETED.value, "payload": payload}
+        await self.rmq_producer.publish(event)
+        logger.info(f"Событие о удалении встречи {meeting.id} отправлено в сервисе Calendar Service")
+
+    async def update_event(self, meeting: Meeting) -> None:
+        """Обновление события в Calendar Service"""
+        payload = MeetingCreatedEvent.model_validate(meeting, by_alias=True).model_dump(mode="json")
+        event = {"type": EventType.UPDATED.value, "payload": payload}
+        await self.rmq_producer.publish(event)
+        logger.info(f"Событие об обновлении встречи {meeting.id} отправлено в сервисе Calendar Service")
+
+    # TODO: Придумать как сделать проверку события через RabbitMQ
     async def has_events_in_period(self, meeting: Meeting, user_id: int) -> bool:
         """Проверка на существование события в Calendar Service"""
         event_data = NewEventHook.model_validate(meeting, by_alias=True).model_dump(
@@ -25,12 +44,3 @@ class EventMixin:
         )
         event_data["employee_id"] = user_id
         return await self.calendar_client.has_events_in_period(event_data)
-
-    async def delete_event(self, meeting: Meeting, user_id: int):
-        """Удаление события из Calendar Service"""
-        event_data = NewEventHook.model_validate(meeting, by_alias=True).model_dump(
-            mode="json", include=["source_id", "event_type"]
-        )
-        event_data["employee_id"] = user_id
-        await self.calendar_client.delete_meeting_webhook(event_data)
-        logger.info(f"Удаление события для встречи {meeting.id=} в Calendar Service")
