0b47ab33796d6090f03f2fbe0d8dcb58f8ed229f
bissaliev <bissaliev21@gmail.com>
2025-05-14 13:59:19 +0400

    meeting_service: Подключил EventConsumer в главный модуль через lifespan

--- meetings_service/app/main.py
+++ meetings_service/app/main.py
@@ -1,8 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.clients.rabbitmq.event_publisher import EventPublisher
 from app.routers.meetings import router as meeting_router
 
-app = FastAPI(root_path="/meeting-service")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Подключение клиента для брокера сообщений RabbitMQ"""
+    app.rmq_producer = EventPublisher("calendar_events")
+    await app.rmq_producer.start_connection()
+    yield
+    await app.rmq_producer.disconnect()
+
+
+app = FastAPI(root_path="/meeting-service", lifespan=lifespan)
 
 
 app.include_router(meeting_router, prefix="/meetings", tags=["meetings"])
